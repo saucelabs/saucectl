473aa4488aafb4193a316f5f7e72df5540124dee
vrunoa <vruno.alassia@gmail.com>
2020-05-05 22:59:24 -0700

    refactor spinner

--- cli/progress/progress.go
+++ cli/progress/progress.go
@@ -3,48 +3,22 @@ package progress
 import (
 	"fmt"
 	"time"
-	"sync"
 
 	"github.com/briandowns/spinner"
 )
 
-type SpinnerSingleton struct {
-	Spinner *spinner.Spinner
-	Speed time.Duration
-	Animation []string
-}
+var spinnerSpeed = 300 * time.Millisecond
+var spinnerInstance *spinner.Spinner = spinner.New(spinner.CharSets[14], spinnerSpeed)
 
-var lock = &sync.Mutex{}
-var spinnerInstance *SpinnerSingleton
-
-func NewSpinner() (*SpinnerSingleton) {
-	/* Using a singleton to stop previous messages
-	 * otherwise multiple message can be displayed at the same time 
-	 * if we forget to run spinner.Stop()
-	*/
-	if spinnerInstance == nil {
-		lock.Lock()
-		defer lock.Unlock()
-		if spinnerInstance == nil {
-			spinnerInstance = &SpinnerSingleton{}
-			spinnerInstance.Speed = 300*time.Millisecond
-			spinnerInstance.Animation = []string{"⠋ ", "⠙ ", "⠹ ","⠸ ","⠼ ","⠴ ", "⠦ ", "⠧ ", "⠇ ", "⠏ "}
-			spinnerInstance.Spinner = spinner.New(spinnerInstance.Animation, spinnerInstance.Speed)
-		}
-	}
+func Show(text string, args ...interface{}) *spinner.Spinner {
+	fmt.Println("here2")
+	message := " " + fmt.Sprintf(text, args...)
+	spinnerInstance.Suffix = message
+	spinnerInstance.Stop()
+	spinnerInstance.Start()
 	return spinnerInstance
 }
 
-func Show(text string, args ...interface{}) *spinner.Spinner {
-	message := fmt.Sprintf(text, args...)
-	spinner := NewSpinner()
-	spinner.Spinner.Suffix = message
-	spinner.Spinner.Stop()
-	spinner.Spinner.Start()
-	return spinner.Spinner
-}
-
 func Stop() {
-	spinner := NewSpinner()
-	spinner.Spinner.Stop()
+	spinnerInstance.Stop()
 }
