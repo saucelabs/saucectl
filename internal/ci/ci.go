c12400dd35d174dbd303416fbaf04e0779e6461f
Alex Plischke <alex.plischke@saucelabs.com>
2020-08-17 16:34:28 -0700

    Consolidate statement

--- internal/ci/ci.go
+++ internal/ci/ci.go
@@ -5,10 +5,5 @@ import "os"
 // IsAvailable detects whether this code is executed inside a CI environment.
 func IsAvailable() bool {
 	// Most CI providers have this.
-	if os.Getenv("CI") != "" {
-		return true
-	}
-
-	// Jenkins gets special treatment.
-	return os.Getenv("BUILD_NUMBER") != ""
+	return os.Getenv("CI") != "" || os.Getenv("BUILD_NUMBER") != ""
 }
