d0eaa8f8b028509da47464c30dcf7c358739576c
Alex Plischke <alex.plischke@saucelabs.com>
2020-08-20 09:59:36 -0700

    Update the playwright test

--- tests/e2e/playwright/example.test.js
+++ tests/e2e/playwright/example.test.js
@@ -1,6 +1,5 @@
 describe('saucectl demo test', () => {
 	test('should verify title of the page', async () => {
-		const page = (await browser.pages())[0]
 		await page.goto('https://www.saucedemo.com/');
 		expect(await page.title()).toBe('Swag Labs');
 	});
