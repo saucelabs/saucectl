b461c1b16e1faf362b1d3e3b84a95e763ab8dd16
Alex Plischke <alex.plischke@saucelabs.com>
2020-09-02 13:23:24 -0700

    Add function to list files based on regexp patterns

--- internal/fpath/file.go
+++ internal/fpath/file.go
@@ -2,10 +2,12 @@ package fpath
 
 import (
 	"github.com/rs/zerolog/log"
+	"os"
 	"path/filepath"
+	"regexp"
 )
 
-// Globs returns the names of all files matching the patterns.
+// Globs returns the names of all files matching the glob patterns.
 // Effectively syntactic sugar for filepath.Glob() to support multiple patterns.
 func Globs(patterns []string) []string {
 	var files []string
@@ -20,4 +22,28 @@ func Globs(patterns []string) []string {
 	}
 
 	return files
+}
+
+// List returns a list of files matching the regexp pattern.
+// Unlike filepath.Glob, this method will inspect all subdirectories of dir.
+func List(dir string, pattern string) ([]string, error) {
+	var ll []string
+
+	r, err := regexp.Compile(pattern)
+	if err != nil {
+		return ll, err
+	}
+
+	err = filepath.Walk(dir, func(p string, i os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if r.MatchString(p) {
+			ll = append(ll, p)
+		}
+		return nil
+	})
+
+	return ll, err
 }
