bbf09a48d99c0ad62996e0dc97ebaf6c59e5648f
Dan <dpgraham.85@gmail.com>
2020-09-15 20:01:32 -0700

    Again

--- internal/ci/ci.go
+++ internal/ci/ci.go
@@ -6,6 +6,6 @@ import "os"
 func IsAvailable() bool {
 	// Most CI providers have this.
 	isCi := os.Getenv("CI") != "" || os.Getenv("BUILD_NUMBER") != ""
-	shouldSkipCi := os.Getenv("SKIP_CI") != ""
-	return isCi && !shouldSkipCi
+	skip := os.Getenv("SKIP_CI") != ""
+	return isCi && !skip
 }
