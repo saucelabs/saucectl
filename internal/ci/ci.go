a2904a488a756cdd1dd16ce15002444a4615796e
Dan Graham <dan@saucelabs.com>
2020-09-15 20:56:18 -0700

    feat: Add SKIP_CI parameter to saucectl (#67)

--- internal/ci/ci.go
+++ internal/ci/ci.go
@@ -2,8 +2,10 @@ package ci
 
 import "os"
 
-// IsAvailable detects whether this code is executed inside a CI environment.
+// IsAvailable detects whether this code is executed inside a CI environment
 func IsAvailable() bool {
 	// Most CI providers have this.
-	return os.Getenv("CI") != "" || os.Getenv("BUILD_NUMBER") != ""
+	isCi := os.Getenv("CI") != "" || os.Getenv("BUILD_NUMBER") != ""
+	skip := os.Getenv("SKIP_CI") != ""
+	return isCi && !skip
 }
