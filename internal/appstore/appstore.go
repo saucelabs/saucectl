1fd4894e6a451a66c92120b87ecb6dde05bcc711
Alex Plischke <alex.plischke@saucelabs.com>
2020-12-01 12:58:50 -0800

    Archive and upload project for cloud (#116)
    * Refactor file uploader interface
    
    * Archive and upload project

--- internal/appstore/appstore.go
+++ internal/appstore/appstore.go
@@ -2,6 +2,11 @@ package appstore
 
 import (
 	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/rs/zerolog/log"
+	"github.com/saucelabs/saucectl/internal/storage"
 	"io"
 	"io/ioutil"
 	"mime/multipart"
@@ -9,11 +14,20 @@ import (
 	"os"
 	"path/filepath"
 	"time"
-
-	"github.com/saucelabs/saucectl/internal/fileuploader"
 )
 
-// AppStore implements functions for AppStore interface
+// UploadResponse represents the response as is returned by the app store.
+type UploadResponse struct {
+	Item Item `json:"item"`
+}
+
+// Item represents the metadata about the uploaded file.
+type Item struct {
+	ID string `json:"id"`
+}
+
+// AppStore implements a remote file storage for storage.ProjectUploader.
+// See https://wiki.saucelabs.com/display/DOCS/Application+Storage for more details.
 type AppStore struct {
 	HTTPClient *http.Client
 	URL        string
@@ -22,9 +36,9 @@ type AppStore struct {
 }
 
 // New returns an implementation for AppStore
-func New(url, username, accessKey string, timeout int) fileuploader.FileUploader {
+func New(url, username, accessKey string, timeout time.Duration) *AppStore {
 	return &AppStore{
-		HTTPClient: &http.Client{Timeout: time.Duration(timeout) * time.Second},
+		HTTPClient: &http.Client{Timeout: timeout},
 		URL:        url,
 		Username:   username,
 		AccessKey:  accessKey,
@@ -32,28 +46,42 @@ func New(url, username, accessKey string, timeout int) fileuploader.FileUploader
 }
 
 // Upload uploads file to remote storage
-func (s *AppStore) Upload(fileName, formType string) error {
-	body, contentType, err := readFile(fileName, formType)
+func (s *AppStore) Upload(name string) (storage.ArtifactMeta, error) {
+	body, contentType, err := readFile(name)
 	if err != nil {
-		return err
+		return storage.ArtifactMeta{}, err
 	}
-
-	request, err := createRequest(s.URL, s.Username, s.AccessKey, body, contentType)
+	
+	request, err := createRequest(fmt.Sprintf("%s/v1/storage/upload", s.URL), s.Username, s.AccessKey, body, contentType)
 	if err != nil {
-		return err
+		return storage.ArtifactMeta{}, err
 	}
 
 	resp, err := s.HTTPClient.Do(request)
 	if err != nil {
-		return err
+		return storage.ArtifactMeta{}, err
 	}
 	defer resp.Body.Close()
 
-	_, err = ioutil.ReadAll(resp.Body)
-	return err
+	if resp.StatusCode != 201 {
+		b, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return storage.ArtifactMeta{}, err
+		}
+		log.Error().Msgf("Failed to upload project. Invalid response %d, body: %v", resp.StatusCode, string(b))
+		return storage.ArtifactMeta{}, errors.New("failed to upload project")
+	}
+
+	var ur UploadResponse
+
+	if err := json.NewDecoder(resp.Body).Decode(&ur); err != nil {
+		return storage.ArtifactMeta{}, err
+	}
+
+	return storage.ArtifactMeta{ID: ur.Item.ID}, err
 }
 
-func readFile(fileName, formType string) (*bytes.Buffer, string, error) {
+func readFile(fileName string) (*bytes.Buffer, string, error) {
 	file, err := os.Open(fileName)
 	if err != nil {
 		return nil, "", err
@@ -63,7 +91,7 @@ func readFile(fileName, formType string) (*bytes.Buffer, string, error) {
 	body := &bytes.Buffer{}
 	writer := multipart.NewWriter(body)
 	defer writer.Close()
-	part, err := writer.CreateFormFile(formType, filepath.Base(file.Name()))
+	part, err := writer.CreateFormFile("payload", filepath.Base(file.Name()))
 	if err != nil {
 		return nil, "", err
 	}
--- internal/cypress/sauce/runner.go
+++ internal/cypress/sauce/runner.go
@@ -2,16 +2,80 @@ package sauce
 
 import (
 	"github.com/rs/zerolog/log"
+	"github.com/saucelabs/saucectl/cli/progress"
+	"github.com/saucelabs/saucectl/internal/archive/zip"
 	"github.com/saucelabs/saucectl/internal/cypress"
+	"github.com/saucelabs/saucectl/internal/jsonio"
+	"github.com/saucelabs/saucectl/internal/storage"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 )
 
 // Runner represents the Sauce Labs cloud implementation for cypress.
 type Runner struct {
 	Project         cypress.Project
+	ProjectUploader storage.ProjectUploader
 }
 
 // RunProject runs the tests defined in cypress.Project.
 func (r *Runner) RunProject() (int, error) {
-	log.Error().Msg("Not yet implemented.")
+	// Archive the project files.
+	tempDir, err := ioutil.TempDir(os.TempDir(), "saucectl-app-payload")
+	if err != nil {
+		return 1, err
+	}
+	defer os.RemoveAll(tempDir)
+
+	zipName, err := r.archiveProject(tempDir)
+	if err != nil {
+		return 1, err
+	}
+
+	r.uploadProject(zipName)
+	log.Error().Msg("Not yet implemented.") // TODO remove debug
 	return 1, nil
+}
+
+func (r *Runner) archiveProject(tempDir string) (string, error) {
+	zipName := filepath.Join(tempDir, "app.zip")
+	z, err := zip.NewWriter(zipName)
+	if err != nil {
+		return "", err
+	}
+	defer z.Close()
+
+	files := []string{
+		r.Project.Cypress.ConfigFile,
+		r.Project.Cypress.ProjectPath,
+	}
+
+	if r.Project.Cypress.EnvFile != "" {
+		files = append(files, r.Project.Cypress.EnvFile)
+	}
+
+	rcPath := filepath.Join(tempDir, "sauce-runner.json")
+	if err := jsonio.WriteFile(rcPath, r.Project); err != nil {
+		return "", err
+	}
+	files = append(files, rcPath)
+
+	for _, f := range files {
+		if err := z.Add(f, ""); err != nil {
+			return "", err
+		}
+	}
+
+	return zipName, z.Close()
+}
+
+func (r *Runner) uploadProject(filename string) (string, error) {
+	progress.Show("Uploading project")
+	resp, err := r.ProjectUploader.Upload(filename)
+	progress.Stop()
+	if err != nil {
+		return "", err
+	}
+	log.Info().Str("fileID", resp.ID).Msg("Project uploaded.")
+	return resp.ID, nil
 }
