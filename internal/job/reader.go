05a94ca0a6fcbc518fff937fc0344d08fd461575
Alex Plischke <alex.plischke@saucelabs.com>
2020-12-03 15:00:40 -0800

    Describe interface

--- internal/job/reader.go
+++ internal/job/reader.go
@@ -7,6 +7,9 @@ import (
 
 // Reader is the interface for retrieving jobs.
 type Reader interface {
+	// ReadJob returns the job details.
 	ReadJob(ctx context.Context, id string) (Job, error)
+
+	// PollJob polls job details at an interval, until the job has ended, whether successfully or due to an error.
 	PollJob(ctx context.Context, id string, interval time.Duration) (Job, error)
 }
