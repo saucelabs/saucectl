a91740df5409efc48b1c5c5a9061566158234e60
Alex Plischke <alex.plischke@saucelabs.com>
2020-12-08 16:26:27 +0100

    Allow the user the specify a tunnel (#126)

--- internal/job/starter.go
+++ internal/job/starter.go
@@ -2,19 +2,26 @@ package job
 
 import "context"
 
-// StartOptions represents the options for starting a job.
+// StartOptions represents the options for starting a job in the Sauce Labs cloud.
 type StartOptions struct {
-	User           string   `json:"username"`
-	AccessKey      string   `json:"accessKey"`
-	App            string   `json:"app,omitempty"`
-	Suite          string   `json:"suite,omitempty"`
-	Framework      string   `json:"framework,omitempty"`
-	BrowserName    string   `json:"browserName,omitempty"`
-	BrowserVersion string   `json:"browserVersion,omitempty"`
-	PlatformName   string   `json:"platformName,omitempty"`
-	Name           string   `json:"name,omitempty"`
-	Build          string   `json:"build,omitempty"`
-	Tags           []string `json:"tags,omitempty"`
+	User           string        `json:"username"`
+	AccessKey      string        `json:"accessKey"`
+	App            string        `json:"app,omitempty"`
+	Suite          string        `json:"suite,omitempty"`
+	Framework      string        `json:"framework,omitempty"`
+	BrowserName    string        `json:"browserName,omitempty"`
+	BrowserVersion string        `json:"browserVersion,omitempty"`
+	PlatformName   string        `json:"platformName,omitempty"`
+	Name           string        `json:"name,omitempty"`
+	Build          string        `json:"build,omitempty"`
+	Tags           []string      `json:"tags,omitempty"`
+	Tunnel         TunnelOptions `json:"tunnel,omitempty"`
+}
+
+// TunnelOptions represents the options that configure the usage of a tunnel when running tests in the Sauce Labs cloud.
+type TunnelOptions struct {
+	ID     string `json:"id"`
+	Parent string `json:"parent,omitempty"`
 }
 
 // Starter is the interface for starting jobs.
