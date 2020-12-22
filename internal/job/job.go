469adefb14bfe240b10f18b52e4a740935610fe9
Félix P <felix@saucelabs.com>
2020-12-22 09:46:40 -0800

    Capture TestComposer jobID from API correctly

--- internal/job/job.go
+++ internal/job/job.go
@@ -16,7 +16,7 @@ var DoneStates = []string{StateComplete, StateError}
 // Job represents test details and metadata of a test run (aka Job), that is usually associated with a particular test
 // execution instance (e.g. VM).
 type Job struct {
-	ID     string `json:"id"`
+	ID     string `json:"jobID"`
 	Passed bool   `json:"passed"`
 	Status string `json:"status"`
 	Error  string `json:"error"`
