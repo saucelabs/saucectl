8c813fc5ed739d0797a5bcf5862e643292bc80f2
Félix P <felix@saucelabs.com>
2021-01-18 11:26:24 -0800

    Send Framework version to backend

--- internal/job/starter.go
+++ internal/job/starter.go
@@ -4,18 +4,19 @@ import "context"
 
 // StartOptions represents the options for starting a job in the Sauce Labs cloud.
 type StartOptions struct {
-	User           string        `json:"username"`
-	AccessKey      string        `json:"accessKey"`
-	App            string        `json:"app,omitempty"`
-	Suite          string        `json:"suite,omitempty"`
-	Framework      string        `json:"framework,omitempty"`
-	BrowserName    string        `json:"browserName,omitempty"`
-	BrowserVersion string        `json:"browserVersion,omitempty"`
-	PlatformName   string        `json:"platformName,omitempty"`
-	Name           string        `json:"name,omitempty"`
-	Build          string        `json:"build,omitempty"`
-	Tags           []string      `json:"tags,omitempty"`
-	Tunnel         TunnelOptions `json:"tunnel,omitempty"`
+	User             string        `json:"username"`
+	AccessKey        string        `json:"accessKey"`
+	App              string        `json:"app,omitempty"`
+	Suite            string        `json:"suite,omitempty"`
+	Framework        string        `json:"framework,omitempty"`
+	FrameworkVersion string        `json:"frameworkVersion,omitempty"`
+	BrowserName      string        `json:"browserName,omitempty"`
+	BrowserVersion   string        `json:"browserVersion,omitempty"`
+	PlatformName     string        `json:"platformName,omitempty"`
+	Name             string        `json:"name,omitempty"`
+	Build            string        `json:"build,omitempty"`
+	Tags             []string      `json:"tags,omitempty"`
+	Tunnel           TunnelOptions `json:"tunnel,omitempty"`
 }
 
 // TunnelOptions represents the options that configure the usage of a tunnel when running tests in the Sauce Labs cloud.
