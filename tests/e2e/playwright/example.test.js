8be7738f0ea670f4db8caf5a268b9fb0a56485bf
Félix P <felix@saucelabs.com>
2021-01-20 15:55:11 -0800

    Update test for playwright

--- tests/e2e/playwright/example.test.js
+++ tests/e2e/playwright/example.test.js
@@ -1,6 +1,6 @@
-describe('saucectl demo test', () => {
-	test('should verify title of the page', async () => {
-		await page.goto('https://www.saucedemo.com/');
-		expect(await page.title()).toBe('Swag Labs');
-	});
+const { it, expect } = require('@playwright/test');
+
+it('should verify title of the page', async ({ page }) => {
+	await page.goto('https://www.saucedemo.com/');
+	expect(await page.title()).toBe('Swag Labs');
 });
