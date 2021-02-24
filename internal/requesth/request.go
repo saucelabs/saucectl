0825a56bc5ea80e91fdf86faac8fa9352f912499
Alex Plischke <alex.plischke@saucelabs.com>
2021-02-24 09:57:59 -0800

    Fix lint

--- internal/requesth/request.go
+++ internal/requesth/request.go
@@ -7,6 +7,8 @@ import (
 	"net/http"
 )
 
+// NewWithContext is a wrapper around http.NewRequestWithContext that modifies the request by adding additional
+// headers.
 func NewWithContext(ctx context.Context, method, url string, body io.Reader) (*http.Request, error) {
 	r, err := http.NewRequestWithContext(ctx, method, url, body)
 	if err != nil {
@@ -17,12 +19,7 @@ func NewWithContext(ctx context.Context, method, url string, body io.Reader) (*h
 	return r, err
 }
 
+// New is a wrapper around NewWithConext.
 func New(method, url string, body io.Reader) (*http.Request, error) {
-	r, err := http.NewRequest(method, url, body)
-	if err != nil {
-		return r, err
-	}
-	r.Header.Set("User-Agent", "saucectl/"+version.Version)
-
-	return r, err
+	return NewWithContext(context.Background(), method, url, body)
 }
