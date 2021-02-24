7423c477ce13b286cca421bdd9a35dddf52bb07d
Alex Plischke <alex.plischke@saucelabs.com>
2021-02-24 11:05:52 -0800

    Add tests

---
+++ internal/requesth/request_test.go
@@ -0,0 +1,83 @@
+package requesth
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+func TestNewWithContext(t *testing.T) {
+	type args struct {
+		ctx    context.Context
+		method string
+		url    string
+		body   io.Reader
+	}
+	tests := []struct {
+		name        string
+		args        args
+		wantHeaders http.Header
+		wantErr     bool
+	}{
+		{
+			name: "expect headers",
+			args: args{
+				ctx:    context.Background(),
+				method: "GET",
+				url:    "http://localhost",
+				body:   nil,
+			},
+			wantHeaders: http.Header{"User-Agent": []string{"saucectl/v0.0.0+unknown"}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := NewWithContext(tt.args.ctx, tt.args.method, tt.args.url, tt.args.body)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewWithContext() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got.Header, tt.wantHeaders) {
+				t.Errorf("Headers got = %v, want %v", got, tt.wantHeaders)
+			}
+		})
+	}
+}
+
+func TestNew(t *testing.T) {
+	type args struct {
+		method string
+		url    string
+		body   io.Reader
+	}
+	tests := []struct {
+		name        string
+		args        args
+		wantHeaders http.Header
+		wantErr     bool
+	}{
+		{
+			name: "expect headers",
+			args: args{
+				method: "GET",
+				url:    "http://localhost",
+				body:   nil,
+			},
+			wantHeaders: http.Header{"User-Agent": []string{"saucectl/v0.0.0+unknown"}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := New(tt.args.method, tt.args.url, tt.args.body)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("New() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got.Header, tt.wantHeaders) {
+				t.Errorf("Headers got = %v, want %v", got, tt.wantHeaders)
+			}
+		})
+	}
+}
