9dcd860a97f35c6d61169c5310bae60cad6e8c79
Alex Plischke <alex.plischke@saucelabs.com>
2021-03-29 13:09:01 -0700

    Clean up legacy configuration

--- internal/mocks/runner.go
+++ internal/mocks/runner.go
@@ -1,22 +1,11 @@
 package mocks
 
-import (
-	"github.com/saucelabs/saucectl/cli/command"
-	"github.com/saucelabs/saucectl/internal/config"
-)
-
 // TestRunner is a mock to test runner functionalities
 type TestRunner struct {
 	HasProject  bool
 	HasSetup    bool
 	HasRun      bool
 	HasFinished bool
-}
-
-// NewTestRunner creates a runner for unit testing purposes
-func NewTestRunner(c config.Project, cli *command.SauceCtlCli) (*TestRunner, error) {
-	runner := TestRunner{}
-	return &runner, nil
 }
 
 // RunProject pretends to run tests defined by config.Project.
--- internal/runner/runner.go
+++ internal/runner/runner.go
@@ -1,44 +1,6 @@
 package runner
 
-import (
-	"context"
-	"github.com/saucelabs/saucectl/cli/command"
-	"github.com/saucelabs/saucectl/internal/config"
-	"github.com/saucelabs/saucectl/internal/utils"
-)
-
-const logDir = "/var/log/cont"
-
-var homeDir = utils.GetProjectDir()
-
-// ConfigPath represents the path for the runner config.
-var ConfigPath = homeDir + "/config.yaml"
-
-// LogFiles contains the locations of log and resource files that are useful for reporting.
-var LogFiles = []string{
-	logDir + "/chrome_browser.log",
-	logDir + "/firefox_browser.log",
-	logDir + "/supervisord.log",
-	logDir + "/video-rec-stderr.log",
-	logDir + "/video-rec-stdout.log",
-	logDir + "/wait-xvfb.1.log",
-	logDir + "/wait-xvfb.2.log",
-	logDir + "/wait-xvfb-stdout.log",
-	logDir + "/xvfb-tryouts-stderr.log",
-	logDir + "/xvfb-tryouts-stdout.log",
-	homeDir + "/videos/video.mp4",
-	homeDir + "/docker.log",
-}
-
 // Testrunner describes the test runner interface
 type Testrunner interface {
 	RunProject() (int, error)
-}
-
-// BaseRunner contains common properties across all runners
-type BaseRunner struct {
-	Project      config.Project
-	RunnerConfig config.RunnerConfiguration
-	Ctx          context.Context
-	Cli          *command.SauceCtlCli
 }
