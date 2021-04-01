c19f1d15277db3466b1a2c0673de6d9ad9b7da92
Alex Plischke <alex.plischke@saucelabs.com>
2021-04-01 16:40:30 -0700

    Add deprecation marker

--- internal/testcafe/config.go
+++ internal/testcafe/config.go
@@ -58,7 +58,7 @@ type Screenshots struct {
 
 // Testcafe represents the configuration for testcafe.
 type Testcafe struct {
-	// ProjectPath is the path for testing project.
+	// Deprecated. ProjectPath is succeeded by Project.RootDir.
 	ProjectPath string `yaml:"projectPath,omitempty" json:"projectPath"`
 
 	// Version represents the testcafe framework version.
@@ -82,8 +82,7 @@ func FromFile(cfgPath string) (Project, error) {
 		return p, fmt.Errorf("could not find 'rootDir' in config yml, 'rootDir' must be set to specify project files")
 	} else if p.Testcafe.ProjectPath != "" && p.RootDir == "" {
 		log.Warn().Msg("'testcafe.projectPath' is deprecated. Consider using 'rootDir' instead")
-		//p.RootDir = p.Testcafe.ProjectPath
-		p.RootDir = "."
+		p.RootDir = p.Testcafe.ProjectPath
 	} else if p.Testcafe.ProjectPath != "" && p.RootDir != "" {
 		log.Info().Msgf(
 			"Found both 'testcafe.projectPath=%s' and 'rootDir=%s' in config. 'projectPath' is deprecated, so defaulting to rootDir '%s'",
