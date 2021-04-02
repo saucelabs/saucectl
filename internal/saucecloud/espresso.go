465c4f34e73af8f0f167021675ebfd6e5f4530a1
vrunoa <vruno.alassia@gmail.com>
2021-04-01 20:48:41 -0700

    schedule jobs multiple by suites*devices*platforms

--- internal/saucecloud/espresso.go
+++ internal/saucecloud/espresso.go
@@ -2,6 +2,7 @@ package saucecloud
 
 import (
 	"fmt"
+	"github.com/rs/zerolog/log"
 	"github.com/saucelabs/saucectl/internal/espresso"
 	"github.com/saucelabs/saucectl/internal/job"
 )
@@ -46,25 +47,47 @@ func (r *EspressoRunner) runSuites(appFileId string, testAppFileId string) bool 
 	defer close(results)
 
 	// Submit suites to work on.
+	jobsCount := r.calculateJobsCount(r.Project.Suites)
 	go func() {
 		for _, s := range r.Project.Suites {
-			jobOpts <- job.StartOptions{
-				App:              fmt.Sprintf("storage:%s", appFileId),
-				Suite:            fmt.Sprintf("storage:%s", testAppFileId),
-				Framework:        "espresso",
-				PlatformName:     s.PlatformName,
-				Name:             r.Project.Sauce.Metadata.Name + " - " + s.Name,
-				Build:            r.Project.Sauce.Metadata.Build,
-				Tags:             r.Project.Sauce.Metadata.Tags,
-				Tunnel: job.TunnelOptions{
-					ID:     r.Project.Sauce.Tunnel.ID,
-					Parent: r.Project.Sauce.Tunnel.Parent,
-				},
-				Experiments:      r.Project.Sauce.Experiments,
+			for _, d := range s.Devices {
+				for _, p := range d.PlatformVersions {
+					log.Info().Str("suite", s.Name).Str("device", d.Name).Str("platform", p).Msg("Starting job")
+					jobsCount++
+					jobOpts <- job.StartOptions{
+						App:              	fmt.Sprintf("storage:%s", appFileId),
+						Suite:            	fmt.Sprintf("storage:%s", testAppFileId),
+						Framework:        	"espresso",
+						FrameworkVersion:   "1.0.0-stable",
+						PlatformName:     	d.PlatformName,
+						PlatformVersion:	p,
+						DeviceName:			d.Name,
+						Name:             	r.Project.Sauce.Metadata.Name + " - " + s.Name,
+						Build:            	r.Project.Sauce.Metadata.Build,
+						Tags:             	r.Project.Sauce.Metadata.Tags,
+						Tunnel: job.TunnelOptions{
+							ID:     r.Project.Sauce.Tunnel.ID,
+							Parent: r.Project.Sauce.Tunnel.Parent,
+						},
+						Experiments:      r.Project.Sauce.Experiments,
+					}
+				}
 			}
 		}
 		close(jobOpts)
 	}()
 
-	return r.collectResults(results, len(r.Project.Suites))
+	return r.collectResults(results, jobsCount)
+}
+
+func (r *EspressoRunner) calculateJobsCount(suites []espresso.Suite) int {
+	jobsCount := 0
+	for _, s := range suites {
+		for _, d := range s.Devices {
+			for _, _ = range d.PlatformVersions {
+				jobsCount++
+			}
+		}
+	}
+	return jobsCount
 }
