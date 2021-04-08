c02ab1bb4f02b50d67d0546141022beae1481e18
Alex Plischke <alex.plischke@saucelabs.com>
2021-04-08 09:03:56 -0700

    Add missing close

--- internal/credentials/credentials.go
+++ internal/credentials/credentials.go
@@ -56,6 +56,7 @@ func fromFile(path string) Credentials {
 		log.Error().Msgf("failed to read credentials: %v", err)
 		return Credentials{}
 	}
+	defer yamlFile.Close()
 
 	var c Credentials
 	if err = yamlbase.NewDecoder(yamlFile).Decode(&c); err != nil {
