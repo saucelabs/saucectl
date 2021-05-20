401f1a98bac04178ca2ef9f4dc9599bec21804b3
Valentyn Kremeshnyi <valentyn.kremeshnyi@saucelabs.com>
2021-05-20 14:32:39 -0700

    add unit tests

--- internal/xcuit/config.go
+++ internal/xcuit/config.go
@@ -1,7 +1,13 @@
 package xcuit
 
 import (
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+
 	"github.com/saucelabs/saucectl/internal/config"
+	"gopkg.in/yaml.v2"
 )
 
 type deviceType string
@@ -54,4 +60,75 @@ type Options struct {
 	CarrierConnectivity *bool      `yaml:"carrierConnectivity,omitempty" json:"carrierConnectivity"`
 	DeviceType          deviceType `yaml:"deviceType,omitempty" json:"deviceType"`
 	Private             *bool      `yaml:"private,omitempty" json:"private"`
+}
+
+// FromFile creates a new xcuit Project based on the filepath cfgPath.
+func FromFile(cfgPath string) (Project, error) {
+	var p Project
+
+	f, err := os.Open(cfgPath)
+	if err != nil {
+		return Project{}, fmt.Errorf("failed to locate project config: %v", err)
+	}
+	defer f.Close()
+
+	if err := yaml.NewDecoder(f).Decode(&p); err != nil {
+		return Project{}, fmt.Errorf("failed to parse project config: %v", err)
+	}
+	p.ConfigFilePath = cfgPath
+
+	if p.Sauce.Concurrency < 1 {
+		// Default concurrency is 2
+		p.Sauce.Concurrency = 2
+	}
+
+	return p, nil
+}
+
+// Validate validates basic configuration of the project and returns an error if any of the settings contain illegal
+// values. This is not an exhaustive operation and further validation should be performed both in the client and/or
+// server side depending on the workflow that is executed.
+func Validate(p Project) error {
+	if p.Xcuit.App == "" {
+		return errors.New("missing path to app .ipa")
+	}
+	if !strings.HasSuffix(p.Xcuit.App, ".ipa") {
+		return fmt.Errorf("invaild application file: %s, make sure extension is .ipa", p.Xcuit.App)
+	}
+	_, err := os.Stat(p.Xcuit.App)
+	if os.IsNotExist(err) {
+		return fmt.Errorf("application file: %s does not exists", p.Xcuit.App)
+	}
+
+	if p.Xcuit.TestApp == "" {
+		return errors.New("missing path to the bundle with tests")
+	}
+	_, err = os.Stat(p.Xcuit.TestApp)
+	if os.IsNotExist(err) {
+		return fmt.Errorf("bundle with tests: %s does not exists", p.Xcuit.TestApp)
+	}
+
+	if len(p.Suites) == 0 {
+		return errors.New("no suites defined")
+	}
+
+	for _, suite := range p.Suites {
+		if len(suite.Devices) == 0 {
+			return fmt.Errorf("missing devices configuration for suite: %s", suite.Name)
+		}
+		//for didx, device := range suite.Devices {
+		//	if device.Name == "" {
+		//		return fmt.Errorf("missing device name for suite: %s. Devices index: %d", suite.Name, didx)
+		//	}
+		//	if !strings.Contains(strings.ToLower(device.Name), "emulator") {
+		//		return fmt.Errorf("missing `emulator` in device name: %s, real device cloud is unsupported right now", device.Name)
+		//	}
+		//	if len(device.PlatformVersions) == 0 {
+		//		// TODO - update message when handling device.Id
+		//		return fmt.Errorf("missing platform versions for device: %s", device.Name)
+		//	}
+		//}
+	}
+
+	return nil
 }
--- internal/xcuit/config_test.go
+++ internal/xcuit/config_test.go
@@ -1,1 +1,160 @@
 package xcuit
+
+import (
+	"errors"
+	"path/filepath"
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"gotest.tools/v3/fs"
+)
+
+func TestValidate(t *testing.T) {
+	dir := fs.NewDir(t, "xcuit-config",
+		fs.WithFile("test.ipa", "", fs.WithMode(0655)),
+		fs.WithDir("test.app", fs.WithMode(0655)))
+	defer dir.Remove()
+	appF := filepath.Join(dir.Path(), "test.ipa")
+	appBundle := filepath.Join(dir.Path(), "test.app")
+
+	testCases := []struct {
+		name        string
+		p           *Project
+		expectedErr error
+	}{
+		{
+			name:        "validating throws error on empty app",
+			p:           &Project{},
+			expectedErr: errors.New("missing path to app .ipa"),
+		},
+		{
+			name: "validating throws error on app missing .ipa",
+			p: &Project{
+				Xcuit: Xcuit{
+					App: "/path/to/app",
+				},
+			},
+			expectedErr: errors.New("invaild application file: /path/to/app, make sure extension is .ipa"),
+		},
+		{
+			name: "validating throws error on not exists .ipa file",
+			p: &Project{
+				Xcuit: Xcuit{
+					App: "/path/to/app/test.ipa",
+				},
+			},
+			expectedErr: errors.New("application file: /path/to/app/test.ipa does not exists"),
+		},
+		{
+			name: "validating throws error on empty testApp",
+			p: &Project{
+				Xcuit: Xcuit{
+					App:     appF,
+					TestApp: "",
+				},
+			},
+			expectedErr: errors.New("missing path to the bundle with tests"),
+		},
+		{
+			name: "validating throws error on not exists bundle with tests",
+			p: &Project{
+				Xcuit: Xcuit{
+					App:     appF,
+					TestApp: "/path/to/bundle/tests",
+				},
+			},
+			expectedErr: errors.New("bundle with tests: /path/to/bundle/tests does not exists"),
+		},
+		{
+			name: "validating throws error on missing suites",
+			p: &Project{
+				Xcuit: Xcuit{
+					App:     appF,
+					TestApp: appBundle,
+				},
+			},
+			expectedErr: errors.New("no suites defined"),
+		},
+		{
+			name: "validating throws error on missing devices",
+			p: &Project{
+				Xcuit: Xcuit{
+					App:     appF,
+					TestApp: appBundle,
+				},
+				Suites: []Suite{
+					Suite{
+						Name:    "no devices",
+						Devices: []Device{},
+					},
+				},
+			},
+			expectedErr: errors.New("missing devices configuration for suite: no devices"),
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			err := Validate(*tc.p)
+			assert.NotNil(t, err)
+			assert.Equal(t, err.Error(), tc.expectedErr.Error())
+		})
+	}
+}
+
+func TestFromFile(t *testing.T) {
+	dir := fs.NewDir(t, "xcuit-cfg",
+		fs.WithFile("config.yml", `apiVersion: v1alpha
+kind: xcuit
+xcuit:
+  app: "./tests/apps/xcuit/SauceLabs.Mobile.Sample.XCUITest.App.ipa"
+  testApp: "./tests/apps/xcuit/SwagLabsMobileAppUITests-Runner.app"
+suites:
+  - name: "saucy barista"
+    devices:
+      - name: "iPhone XR"
+        platformVersion: "14.3"
+    testOptions:
+      - class: "SwagLabsMobileAppUITests.LoginTests"
+        method: "testSuccessfulLogin"
+      - class: "SwagLabsMobileAppUITests.LoginTests"
+`, fs.WithMode(0655)))
+	defer dir.Remove()
+
+	cfg, err := FromFile(filepath.Join(dir.Path(), "config.yml"))
+	if err != nil {
+		t.Errorf("expected error: %v, got: %v", nil, err)
+	}
+	expected := Project{
+		Xcuit: Xcuit{
+			App:     "./tests/apps/xcuit/SauceLabs.Mobile.Sample.XCUITest.App.ipa",
+			TestApp: "./tests/apps/xcuit/SwagLabsMobileAppUITests-Runner.app",
+		},
+		Suites: []Suite{
+			{
+				Name: "saucy barista",
+				Devices: []Device{
+					{
+						Name:            "iPhone XR",
+						PlatformVersion: "14.3",
+					},
+				},
+				TestOptions: []TestOption{
+					TestOption{
+						Class:  "SwagLabsMobileAppUITests.LoginTests",
+						Method: "testSuccessfulLogin",
+					},
+					TestOption{
+						Class: "SwagLabsMobileAppUITests.LoginTests",
+					},
+				},
+			},
+		},
+	}
+	if !reflect.DeepEqual(cfg.Xcuit, expected.Xcuit) {
+		t.Errorf("expected: %v, got: %v", expected, cfg)
+	}
+	if !reflect.DeepEqual(cfg.Suites, expected.Suites) {
+		t.Errorf("expected: %v, got: %v", expected, cfg)
+	}
+}
