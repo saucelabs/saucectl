846e44a6fe3997626f664692c1587ba821e1a871
Valentyn Kremeshnyi <valentyn.kremeshnyi@saucelabs.com>
2021-05-19 17:04:20 -0700

    add xcuit config

---
+++ internal/xcuit/config.go
@@ -0,0 +1,57 @@
+package xcuit
+
+import (
+	"github.com/saucelabs/saucectl/internal/config"
+)
+
+type deviceType string
+
+const (
+	ANY    deviceType = "any"
+	PHONE  deviceType = "phone"
+	TABLET deviceType = "tablet"
+)
+
+// Project represents the xcuit project configuration.
+type Project struct {
+	config.TypeDef `yaml:",inline"`
+	ConfigFilePath string             `yaml:"-" json:"-"`
+	Sauce          config.SauceConfig `yaml:"sauce,omitempty" json:"sauce"`
+	Xcuit          Xcuit              `yaml:"espresso,omitempty" json:"espresso"`
+	Suites         []Suite            `yaml:"suites,omitempty" json:"suites"`
+	Artifacts      config.Artifacts   `yaml:"artifacts,omitempty" json:"artifacts"`
+}
+
+// Xcuit represents xcuit apps configuration.
+type Xcuit struct {
+	App     string `yaml:"app,omitempty" json:"app"`
+	TestApp string `yaml:"testApp,omitempty" json:"testApp"`
+}
+
+// TestOptions represents the espresso test filter options configuration.
+type TestOption struct {
+	Class  string `yaml:"class,omitempty" json:"class"`
+	Method string `yaml:"method,omitempty" json:"method"`
+}
+
+// Suite represents the espresso test suite configuration.
+type Suite struct {
+	Name        string       `yaml:"name,omitempty" json:"name"`
+	Devices     []Device     `yaml:"devices,omitempty" json:"devices"`
+	TestOptions []TestOption `yaml:"testOptions,omitempty" json:"testOptions"`
+}
+
+// Device represents device configuration.
+type Device struct {
+	ID              string  `yaml:"id,omitempty" json:"id"`
+	Name            string  `yaml:"name,omitempty" json:"name"`
+	PlatformVersion string  `yaml:"platformVersion,omitempty" json:"platformVersion"`
+	Options         Options `yaml:"options,omitempty" json:"options"`
+}
+
+// Options represents device options configuration.
+type Options struct {
+	CarrierConnectivity *bool      `yaml:"carrierConnectivity,omitempty" json:"carrierConnectivity"`
+	DeviceType          deviceType `yaml:"deviceType,omitempty" json:"deviceType"`
+	Private             *bool      `yaml:"private,omitempty" json:"private"`
+}
