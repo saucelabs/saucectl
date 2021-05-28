e07b014cd78ebcf793b3be9d6a4198b268bc072c
Alex Plischke <alex.plischke@saucelabs.com>
2021-05-28 14:18:08 -0700

    Update Puppeteer pipeline

--- tests/e2e/puppeteer-parallel/example-4.test.js
+++
@@ -1,7 +0,0 @@
-describe('saucectl demo test 4', () => {
-	test('should verify title of the page', async () => {
-		const page = (await browser.pages())[0]
-		await page.goto('https://www.saucedemo.com/');
-		expect(await page.title()).toBe('Swag Labs');
-	});
-});
