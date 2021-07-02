09a952256b8d71782846f9428c9df950a821fdeb
Félix P <felix@saucelabs.com>
2021-07-02 23:53:29 +0200

    Define default pattern for Cypress (#360)

--- internal/cmd/ini/cypress.go
+++ internal/cmd/ini/cypress.go
@@ -27,10 +27,13 @@ func configureCypress(cfg *initConfig) interface{} {
 		},
 		Suites: []cypress.Suite{
 			{
-				Name:         fmt.Sprintf("cypress - %s - %s", firstNotEmpty(cfg.platformName, cfg.mode) , cfg.browserName),
+				Name:         fmt.Sprintf("cypress - %s - %s", firstNotEmpty(cfg.platformName, cfg.mode), cfg.browserName),
 				PlatformName: cfg.platformName,
 				Browser:      cfg.browserName,
 				Mode:         cfg.mode,
+				Config: cypress.SuiteConfig{
+					TestFiles: []string{"**/*.*"},
+				},
 			},
 		},
 		Artifacts: config.Artifacts{
