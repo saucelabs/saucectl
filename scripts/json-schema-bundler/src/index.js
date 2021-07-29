2d662d214b9999ed96f6c06c606a35eb93be83a4
Alex Plischke <alex.plischke@saucelabs.com>
2021-07-29 14:03:51 -0700

    [DEVX-892] Add JSON schema for config validation (#390)
    * Add json schema files

---
+++ scripts/json-schema-bundler/src/index.js
@@ -0,0 +1,39 @@
+const $RefParser = require("@apidevtools/json-schema-ref-parser");
+const fs = require('fs');
+const yargs = require('yargs/yargs');
+const {hideBin} = require('yargs/helpers');
+
+const args = yargs(hideBin(process.argv))
+    .command('bundle', 'Resolve all references of the given schema and bundle it into a single file.')
+    .option('schema', {
+        alias: 's',
+        type: 'string',
+        description: 'The input schema filename.'
+    })
+    .option('out', {
+        alias: 'o',
+        type: 'string',
+        description: 'The output schema filename.'
+    })
+    .demandOption(['schema', 'out'])
+    .argv;
+
+$RefParser.bundle(args.s, (err, schema) => {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    } else {
+        // `schema` is just a normal JavaScript object that contains your entire JSON Schema,
+        // including referenced files, combined into a single object.
+        let schemaStr = JSON.stringify(schema, null, 2)
+
+        fs.writeFile(args.o, schemaStr, function (err) {
+            if (err) {
+                console.log(err);
+                process.exit(1)
+            }
+
+            console.log(`Successfully bundled schema: ${args.o}`)
+        });
+    }
+});
