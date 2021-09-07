742ee46ca05d2b11d3ce7732fe1636ca2a32e5ec
Valentyn Kremeshnyi <valentyn.kremeshnyi@saucelabs.com>
2021-09-07 14:13:49 -0700

    fix collectResults

--- internal/docker/container.go
+++ internal/docker/container.go
@@ -314,7 +314,7 @@ func (r *ContainerRunner) runJobs(containerOpts <-chan containerStartOptions, re
 	}
 }
 
-func (r *ContainerRunner) collectResults(artifactCfg config.ArtifactDownload, notifierCfg config.Notifications, results chan result, expected int) bool {
+func (r *ContainerRunner) collectResults(artifactCfg config.ArtifactDownload, results chan result, expected int) bool {
 	// TODO find a better way to get the expected
 	completed := 0
 	inProgress := expected
--- internal/docker/playwright.go
+++ internal/docker/playwright.go
@@ -90,7 +90,7 @@ func (r *PlaywrightRunner) RunProject() (int, error) {
 		close(containerOpts)
 	}()
 
-	hasPassed := r.collectResults(r.Project.Artifacts.Download, r.Project.Notifications, results, len(r.Project.Suites))
+	hasPassed := r.collectResults(r.Project.Artifacts.Download, results, len(r.Project.Suites))
 	if !hasPassed {
 		return 1, nil
 	}
