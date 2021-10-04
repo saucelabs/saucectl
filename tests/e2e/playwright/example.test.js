66948e4b6a9f5e433ce0d57bc6a6d9ffceb334bf
Tian Feng <tian.feng@saucelabs.com>
2021-10-04 13:44:14 -0700

    fix pipeline

--- tests/e2e/playwright/example.test.js
+++ tests/e2e/playwright/example.test.js
@@ -1,6 +1,6 @@
 const { it, expect } = require('@playwright/test');
 
 it('should verify title of the page', async ({ page }) => {
-	await page.goto('https://www.saucedemo.com/');
-	expect(await page.title()).toBe('Swag Labs');
+	await page.goto('https://google.com');
+  await expect(page).toHaveTitle(/Google/);
 });
