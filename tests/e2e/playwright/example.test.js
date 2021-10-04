9b4c78ad38d98925f43a547ce43f1bf747745a35
Tian Feng <tian.feng@saucelabs.com>
2021-10-04 14:07:23 -0700

    fix pipeline

--- tests/e2e/playwright/example.test.js
+++ tests/e2e/playwright/example.test.js
@@ -1,6 +1,6 @@
-const { it, expect } = require('@playwright/test');
+const { test, expect } = require('@playwright/test');
 
-it('should verify title of the page', async ({ page }) => {
-	await page.goto('https://google.com');
-  await expect(page).toHaveTitle(/Google/);
+test('should verify title of the page', async ({ page }) => {
+    await page.goto('https://google.com/');
+    await expect(page).toHaveTitle(/Google/);
 });
