c1933cdac9365b382ecdae3a44063015dec0d166
Alex Plischke <alex.plischke@saucelabs.com>
2022-01-14 08:54:16 -0800

    Codify styles

--- internal/doctor/doctor.go
+++ internal/doctor/doctor.go
@@ -6,6 +6,15 @@ import (
 	"fmt"
 	"github.com/fatih/color"
 	"github.com/saucelabs/saucectl/internal/docker"
+	"strings"
+)
+
+var indent = "    "
+
+var (
+	point = "[•] "
+	fail  = color.RedString("[✖] ")
+	pass  = color.GreenString("[✔] ")
 )
 
 // Verify verifies that all essential dependencies are met.
@@ -25,24 +34,50 @@ func VerifyDocker() error {
 		return err
 	}
 
-	fmt.Println("[•] Docker")
+	printPoint(0, "Docker")
 	if !handler.IsInstalled() {
-		fmt.Printf("    %s Connection: unable to connect to docker. Is it installed & running?\n", color.RedString("[✖]"))
+		printFail(1, "Connection: unable to connect to docker. Is it installed & running?")
 		return errors.New("unable to communicate with docker")
 	}
-	fmt.Printf("    %s Connection\n", color.GreenString("[✔]"))
+	printPass(1, "Connection")
 
 	if err := handler.PullImage(context.Background(), "hello-world"); err != nil {
-		fmt.Printf("    %s Pulling Images: %s\n", color.RedString("[✖]"), err)
+		printFail(1, "Pull Images: %s", err)
 		return err
 	}
-	fmt.Printf("    %s Pull Images\n", color.GreenString("[✔]"))
+	printPass(1, "Pull Images")
 
 	if err := handler.IsLaunchable("hello-world"); err != nil {
-		fmt.Printf("    %s Launch Containers: %s\n", color.RedString("[✖]"), err)
+		printFail(1, "Launch Containers: %s")
 		return err
 	}
-	fmt.Printf("    %s Launch Containers\n", color.GreenString("[✔]"))
+	printPass(1, "Launch Containers")
 
 	return nil
+}
+
+func printPoint(lvl int, msg string, a ...interface{}) {
+	printBullet(lvl, point, msg, a...)
+}
+
+func printFail(lvl int, msg string, a ...interface{}) {
+	printBullet(lvl, fail, msg, a...)
+}
+
+func printPass(lvl int, msg string, a ...interface{}) {
+	printBullet(lvl, pass, msg, a...)
+}
+
+func printBullet(lvl int, bullet, msg string, a ...interface{}) {
+	var b strings.Builder
+
+	for i := 0; i < lvl; i++ {
+		b.WriteString(indent)
+	}
+
+	b.WriteString(bullet)
+
+	_, _ = fmt.Fprintf(&b, msg, a...)
+
+	fmt.Println(b.String())
 }
