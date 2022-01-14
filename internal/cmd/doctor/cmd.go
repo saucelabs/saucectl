f343778ced8bc2bacc3c84973c87979347e5d239
Alex Plischke <alex.plischke@saucelabs.com>
2022-01-13 16:18:16 -0800

    Add doctor command

---
+++ internal/cmd/doctor/cmd.go
@@ -0,0 +1,23 @@
+package doctor
+
+import (
+	"github.com/saucelabs/saucectl/internal/doctor"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+// Command creates the `doctor` command
+func Command() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:          "doctor",
+		Short:        "Runs a series of checks to ensure that your environment is ready to run tests",
+		SilenceUsage: true,
+		Run: func(cmd *cobra.Command, args []string) {
+			if err := doctor.Verify(); err != nil {
+				os.Exit(1)
+			}
+		},
+	}
+
+	return cmd
+}
---
+++ internal/doctor/doctor.go
@@ -0,0 +1,47 @@
+package doctor
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/fatih/color"
+	"github.com/saucelabs/saucectl/internal/docker"
+)
+
+// Verify verifies that all essential dependencies are met.
+func Verify() error {
+	var err error
+	if err = VerifyDocker(); err != nil {
+		return err
+	}
+
+	return err
+}
+
+// VerifyDocker verifies that docker dependencies are met.
+func VerifyDocker() error {
+	handler, err := docker.Create()
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("[•] Docker")
+	if !handler.IsInstalled() {
+		fmt.Printf("    %s Connection: unable to connect to docker. Is it installed & running?\n", color.RedString("[✖]"))
+		return errors.New("unable to communicate with docker")
+	}
+	fmt.Printf("    %s Connection\n", color.GreenString("[✔]"))
+
+	if err := handler.PullImage(context.Background(), "hello-world"); err != nil {
+		fmt.Printf("    %s Pulling Images: %s\n", err, color.RedString("[✖]"))
+		return err
+	}
+	fmt.Printf("    %s Pull Images\n", color.GreenString("[✔]"))
+
+	if err := handler.IsLaunchable("hello-world"); err != nil {
+		fmt.Printf("    %s Launch Containers: %s\n", err, color.RedString("[✖]"))
+	}
+	fmt.Printf("    %s Launch Containers\n", color.GreenString("[✔]"))
+
+	return nil
+}
