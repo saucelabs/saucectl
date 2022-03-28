932047980aab69d18046864371809cfaa4c2f24c
Mike Han <mike.han@saucelabs.com>
2022-03-28 09:35:01 -0600

    find cypress version from package.json

--- internal/cmd/run/cypress.go
+++ internal/cmd/run/cypress.go
@@ -3,6 +3,7 @@ package run
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 	"reflect"
 	"strings"
 
@@ -18,6 +19,7 @@ import (
 	"github.com/saucelabs/saucectl/internal/download"
 	"github.com/saucelabs/saucectl/internal/flags"
 	"github.com/saucelabs/saucectl/internal/msg"
+	"github.com/saucelabs/saucectl/internal/node"
 	"github.com/saucelabs/saucectl/internal/region"
 	"github.com/saucelabs/saucectl/internal/report/captor"
 	"github.com/saucelabs/saucectl/internal/resto"
@@ -127,6 +129,22 @@ func runCypress(cmd *cobra.Command, tc testcomposer.Client, rs resto.Client, as 
 	p.CLIFlags = flags.CaptureCommandLineFlags(cmd.Flags())
 	p.Sauce.Metadata.ExpandEnv()
 	expandReporterConfigEnv(p.Cypress.Reporters)
+
+	// TODO: Resolve cypress version from package.json if necessary
+	if p.Cypress.Version == "package.json" {
+		fp := filepath.Join(p.RootDir, p.Cypress.Version)
+		packageJson, err := node.PackageFromFile(fp)
+		if err != nil {
+			// TODO: Handle err
+		}
+		var ver string
+		var ok bool
+		ver, ok = packageJson.DevDependencies["cypress"]
+		if ok {
+			p.Cypress.Version = ver
+		}
+		// TODO: Handle unresolvable package
+	}
 
 	if err := applyCypressFlags(&p); err != nil {
 		return 1, err
