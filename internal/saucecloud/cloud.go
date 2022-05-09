aecbec9af959a08c8ae14fd72fa3b6b5d72a49e6
Tian Feng <tian.feng@saucelabs.com>
2022-05-09 11:15:59 -0700

    Update internal/saucecloud/cloud.go
    Co-authored-by: Alex Plischke <alex.plischke@saucelabs.com>

--- internal/saucecloud/cloud.go
+++ internal/saucecloud/cloud.go
@@ -277,7 +277,7 @@ func (r *CloudRunner) runJob(opts job.StartOptions) (j job.Job, skipped bool, er
 
 	// Check timeout
 	if j.TimedOut {
-		color.Red("Suite '%s' has reached %.2fs timeout", opts.DisplayName, opts.Timeout.Seconds())
+		color.Red("Suite '%s' has reached timeout of %s", opts.DisplayName, opts.Timeout)
 		if !isRDC {
 			j, err = r.JobStopper.StopJob(context.Background(), id)
 			if err != nil {
