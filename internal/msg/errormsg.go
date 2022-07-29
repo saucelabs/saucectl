34cef7c75c8c31a882e7e12d70fcd8542aacee9e
Mike Han <mike.han@saucelabs.com>
2022-07-29 10:35:56 -0600

    Add a warning if configured visibility is invalid

--- internal/msg/errormsg.go
+++ internal/msg/errormsg.go
@@ -86,6 +86,8 @@ const (
 	MissingDeviceConfig = "missing device name or ID for suite: %s. Devices index: %d"
 	// InvalidDockerFileTransferType indicates illegal file transfer type
 	InvalidDockerFileTransferType = "illegal file transfer type '%s', must be one of '%s'"
+	// InvalidVisibilityWarning indicates that the configured visibility is invalid and has no effect on the test results
+	InvalidVisibilityWarning = "\"%s\" is not a valid visibility value. Setting to default visibility."
 )
 
 // cypress config settings
