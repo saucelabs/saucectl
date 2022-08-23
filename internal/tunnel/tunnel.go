f80f48b9ca7f7fe1487cb7c6e267befd48acfbd8
Mike Han <mike.han@saucelabs.com>
2022-08-23 13:47:22 -0600

    describe tunnel filters

--- internal/tunnel/tunnel.go
+++ internal/tunnel/tunnel.go
@@ -7,10 +7,16 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
+// Filter filters tunnels by type when fetching a user's tunnels.
 type Filter string
 
 const (
+	// NoneFilter is a noop and does no filtering.
 	NoneFilter    Filter = ""
+	// V2AlphaFilter filters down to only tunnels with vm-version: v2alpha when requesting a user's tunnels.
+	// NOTE: We use this filter when checking tunnel readiness for api tests. The response when
+	// requesting a user's tunnel does not include vm-version metadata so we need to use the
+	// filter to ensure the correct type of tunnel has been started for an api test.
 	V2AlphaFilter Filter = "v2alpha"
 )
 
