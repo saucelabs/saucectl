c536a9c2f49d49cf9edd7ddca4136e1bbc351f34
Mike Han <mike.han@saucelabs.com>
2022-10-18 15:07:40 -0600

    add suite for cypress+webkit

--- tests/e2e/cypress.config.js
+++ tests/e2e/cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    experimentalWebKitSupport: true,
     setupNodeEvents (on, config) {
       // implement node event listeners here
     },
--- tests/e2e/cypress_10/e2e/cypress.config.js
+++ tests/e2e/cypress_10/e2e/cypress.config.js
@@ -2,5 +2,6 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
+    experimentalWebKitSupport: true,
   },
 })
