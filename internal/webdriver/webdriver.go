21c18f7b771cc5f58efbbd81bb9e6652868868e4
Mike Han <mike.han@saucelabs.com>
2022-10-25 10:26:22 -0600

    Add prop to set fps

--- internal/webdriver/webdriver.go
+++ internal/webdriver/webdriver.go
@@ -5,14 +5,17 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
+
 	"github.com/saucelabs/saucectl/internal/credentials"
 	"github.com/saucelabs/saucectl/internal/job"
 	"github.com/saucelabs/saucectl/internal/requesth"
 	"github.com/saucelabs/saucectl/internal/slice"
 	"github.com/saucelabs/saucectl/internal/version"
-	"io"
-	"net/http"
 )
+
+const VideoFPS = 25
 
 // Client service
 type Client struct {
@@ -101,6 +104,7 @@ type Batch struct {
 	RunnerVersion    string              `json:"runnerVersion,omitempty"`
 	TestFile         string              `json:"testFile,omitempty"`
 	Args             []map[string]string `json:"args"`
+	VideoFPS         int                 `json:"video_fps"`
 }
 
 // sessionStartResponse represents the response body for starting a session.
@@ -138,6 +142,7 @@ func (c *Client) StartJob(ctx context.Context, opts job.StartOptions) (jobID str
 				RunnerVersion:    opts.RunnerVersion,
 				TestFile:         opts.Suite,
 				Args:             formatEspressoArgs(opts.TestOptions),
+				VideoFPS:         VideoFPS,
 			},
 			IdleTimeout: 9999,
 			MaxDuration: 10800,
