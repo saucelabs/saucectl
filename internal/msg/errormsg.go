8c4818bc7c4df79ad9054291968aff0d3ee0475b
Tian Feng <tian.feng@saucelabs.com>
2023-02-07 09:39:27 -0800

    Update internal/msg/errormsg.go
    Co-authored-by: Alex Plischke <alex.plischke@saucelabs.com>

--- internal/msg/errormsg.go
+++ internal/msg/errormsg.go
@@ -196,6 +196,6 @@ const (
 	RetrieveJobHistoryError = "Unable to retrieve job history. Launching jobs in the default order."
 	// InsightsReportError indicates failed push to insights.
 	InsightsReportError = "unable to report result to insights"
-	// FailedToReadJob indicates failed to read the job details
+	// FailedToReadJob indicates failure to read the job details
 	FailedToReadJob = "unable to read the job %s"
 )
