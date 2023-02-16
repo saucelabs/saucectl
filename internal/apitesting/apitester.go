e51d803324b37cdadcc55e43548e0a0637330f35
Alex Plischke <alex.plischke@saucelabs.com>
2023-02-16 09:48:43 -0800

    Consolidate files

--- internal/apitesting/apitester.go
+++ internal/apitesting/apitester.go
@@ -1,6 +1,7 @@
 package apitesting
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"errors"
@@ -55,6 +56,23 @@ type Project struct {
 type Hook struct {
 	Identifier string `json:"identifier,omitempty"`
 	Name       string `json:"name,omitempty"`
+}
+
+// TestRequest represent a test to be executed
+type TestRequest struct {
+	Name   string            `json:"name"`
+	Tags   []string          `json:"tags"`
+	Input  string            `json:"input"`
+	Unit   string            `json:"unit"`
+	Params map[string]string `json:"params"`
+}
+
+// AsyncResponse describes the json response from the async api endpoints.
+type AsyncResponse struct {
+	ContextIDs []string `json:"contextIds,omitempty"`
+	EventIDs   []string `json:"eventIds,omitempty"`
+	TaskID     string   `json:"taskId,omitempty"`
+	TestIDs    []string `json:"testIds,omitempty"`
 }
 
 // NewAPITester returns a apitesting.APITester
@@ -251,4 +269,127 @@ func (c *APITester) GetHooks(ctx context.Context, projectID string) ([]Hook, err
 		return hooks, err
 	}
 	return hooks, nil
+}
+
+// RunAllAsync runs all the tests for the project described by hookID and returns without waiting for their results.
+func (c *APITester) RunAllAsync(ctx context.Context, hookID string, buildID string, tunnel config.Tunnel, test TestRequest) (AsyncResponse, error) {
+	url := c.composeURL(fmt.Sprintf("/api-testing/rest/v4/%s/tests/_run-all", hookID), buildID, "", tunnel, "")
+
+	payload, err := json.Marshal(test)
+	if err != nil {
+		return AsyncResponse{}, err
+	}
+	payloadReader := bytes.NewReader(payload)
+
+	req, err := requesth.NewWithContext(ctx, http.MethodPost, url, payloadReader)
+	if err != nil {
+		return AsyncResponse{}, err
+	}
+
+	req.SetBasicAuth(c.Username, c.AccessKey)
+
+	resp, err := doAsyncRun(c.HTTPClient, req)
+	if err != nil {
+		return AsyncResponse{}, err
+	}
+	return resp, nil
+}
+
+// RunEphemeralAsync runs the tests for the project described by hookID and returns without waiting for their results.
+func (c *APITester) RunEphemeralAsync(ctx context.Context, hookID string, buildID string, tunnel config.Tunnel, taskID string, test TestRequest) (AsyncResponse, error) {
+	url := c.composeURL(fmt.Sprintf("/api-testing/rest/v4/%s/tests/_exec", hookID), buildID, "", tunnel, "")
+
+	payload, err := json.Marshal(test)
+	if err != nil {
+		return AsyncResponse{}, err
+	}
+	payloadReader := bytes.NewReader(payload)
+
+	req, err := requesth.NewWithContext(ctx, http.MethodPost, url, payloadReader)
+	if err != nil {
+		return AsyncResponse{}, err
+	}
+
+	req.SetBasicAuth(c.Username, c.AccessKey)
+
+	resp, err := doAsyncRun(c.HTTPClient, req)
+	if err != nil {
+		return AsyncResponse{}, err
+	}
+	return resp, nil
+}
+
+// RunTestAsync runs a single test described by testID for the project described by hookID and returns without waiting for results.
+func (c *APITester) RunTestAsync(ctx context.Context, hookID string, testID string, buildID string, tunnel config.Tunnel, test TestRequest) (AsyncResponse, error) {
+	url := c.composeURL(fmt.Sprintf("/api-testing/rest/v4/%s/tests/%s/_run", hookID, testID), buildID, "", tunnel, "")
+
+	payload, err := json.Marshal(test)
+	if err != nil {
+		return AsyncResponse{}, err
+	}
+	payloadReader := bytes.NewReader(payload)
+
+	req, err := requesth.NewWithContext(ctx, http.MethodPost, url, payloadReader)
+	if err != nil {
+		return AsyncResponse{}, err
+	}
+
+	req.SetBasicAuth(c.Username, c.AccessKey)
+
+	resp, err := doAsyncRun(c.HTTPClient, req)
+	if err != nil {
+		return AsyncResponse{}, err
+	}
+
+	return resp, nil
+}
+
+// RunTagAsync runs all the tests for a testTag for a project described by hookID and returns without waiting for results.
+func (c *APITester) RunTagAsync(ctx context.Context, hookID string, testTag string, buildID string, tunnel config.Tunnel, test TestRequest) (AsyncResponse, error) {
+	url := c.composeURL(fmt.Sprintf("/api-testing/rest/v4/%s/tests/_tag/%s/_run", hookID, testTag), buildID, "", tunnel, "")
+
+	payload, err := json.Marshal(test)
+	if err != nil {
+		return AsyncResponse{}, err
+	}
+	payloadReader := bytes.NewReader(payload)
+
+	req, err := requesth.NewWithContext(ctx, http.MethodPost, url, payloadReader)
+	if err != nil {
+		return AsyncResponse{}, err
+	}
+
+	req.SetBasicAuth(c.Username, c.AccessKey)
+
+	resp, err := doAsyncRun(c.HTTPClient, req)
+	if err != nil {
+		return AsyncResponse{}, err
+	}
+	return resp, nil
+}
+
+func doAsyncRun(client *http.Client, request *http.Request) (AsyncResponse, error) {
+	request.Header.Set("Content-Type", "application/json")
+
+	resp, err := client.Do(request)
+	if err != nil {
+		return AsyncResponse{}, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode >= http.StatusInternalServerError {
+		return AsyncResponse{}, errors.New(msg.InternalServerError)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		body, _ := io.ReadAll(resp.Body)
+		return AsyncResponse{}, fmt.Errorf("test execution failed; unexpected response code:'%d', msg:'%v'", resp.StatusCode, string(body))
+	}
+
+	var asyncResponse AsyncResponse
+	if err := json.NewDecoder(resp.Body).Decode(&asyncResponse); err != nil {
+		return AsyncResponse{}, err
+	}
+
+	return asyncResponse, nil
 }
--- internal/apitesting/apitester_test.go
+++ internal/apitesting/apitester_test.go
@@ -460,3 +460,287 @@ func TestAPITester_GetHooks(t *testing.T) {
 		})
 	}
 }
+
+func TestAPITester_RunAllAsync(t *testing.T) {
+	type args struct {
+		ctx     context.Context
+		hookID  string
+		buildID string
+		tunnel  config.Tunnel
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    AsyncResponse
+		wantErr bool
+	}{
+		{
+			name: "Basic trigger",
+			args: args{
+				ctx:    context.Background(),
+				hookID: "dummyHookId",
+			},
+			want: AsyncResponse{
+				ContextIDs: []string{"221270ac-0229-49d1-9025-251a10e9133d"},
+				EventIDs:   []string{"c4ca4238a0b923820dcc509a"},
+				TaskID:     "6ddf80b7-9753-4802-992b-d42948cdb99f",
+				TestIDs:    []string{"c20ad4d76fe97759aa27a0c9"},
+			},
+		},
+	}
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var err error
+		if r.Method != http.MethodPost {
+			w.WriteHeader(http.StatusNotImplemented)
+			return
+		}
+		switch r.URL.Path {
+		case "/api-testing/rest/v4/dummyHookId/tests/_run-all":
+			completeStatusResp := []byte(`{"contextIds":["221270ac-0229-49d1-9025-251a10e9133d"],"eventIds":["c4ca4238a0b923820dcc509a"],"taskId":"6ddf80b7-9753-4802-992b-d42948cdb99f","testIds":["c20ad4d76fe97759aa27a0c9"]}`)
+			_, err = w.Write(completeStatusResp)
+		default:
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+
+		if err != nil {
+			t.Errorf("failed to respond: %v", err)
+		}
+	}))
+	defer ts.Close()
+	c := &APITester{
+		HTTPClient: ts.Client(),
+		URL:        ts.URL,
+		Username:   "dummyUser",
+		AccessKey:  "dummyAccesKey",
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := c.RunAllAsync(tt.args.ctx, tt.args.hookID, tt.args.buildID, tt.args.tunnel, TestRequest{})
+			if (err != nil) != tt.wantErr {
+				t.Errorf("RunAllAsync() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("RunAllAsync() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestAPITester_RunEphemeralAsync(t *testing.T) {
+	type args struct {
+		ctx     context.Context
+		hookID  string
+		buildID string
+		tunnel  config.Tunnel
+		taskID  string
+		test    TestRequest
+	}
+	tests := []struct {
+		name          string
+		args          args
+		assertRequest func(t *testing.T, r *http.Request)
+		reply         []byte
+		want          AsyncResponse
+		wantErr       bool
+	}{
+		{
+			name: "Complete Trigger",
+			args: args{
+				ctx:     context.Background(),
+				hookID:  "dummyHookId",
+				taskID:  "generatedUuid",
+				buildID: "generatedBuildId",
+				tunnel:  config.Tunnel{Name: "tunnelId"},
+				test:    TestRequest{},
+			},
+			assertRequest: func(t *testing.T, r *http.Request) {
+				assert.Equal(t, "/api-testing/rest/v4/dummyHookId/tests/_exec?buildId=generatedBuildId&tunnelId=dummyUser%3AtunnelId", r.RequestURI)
+			},
+			reply: []byte(`{"contextIds":["221270ac-0229-49d1-9025-251a10e9133d"],"eventIds":["c4ca4238a0b923820dcc509a"],"taskId":"6ddf80b7-9753-4802-992b-d42948cdb99f","testIds":["c20ad4d76fe97759aa27a0c9"]}`),
+			want: AsyncResponse{
+				ContextIDs: []string{"221270ac-0229-49d1-9025-251a10e9133d"},
+				EventIDs:   []string{"c4ca4238a0b923820dcc509a"},
+				TaskID:     "6ddf80b7-9753-4802-992b-d42948cdb99f",
+				TestIDs:    []string{"c20ad4d76fe97759aa27a0c9"},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				if r.Method != http.MethodPost {
+					w.WriteHeader(http.StatusNotImplemented)
+					return
+				}
+				switch r.URL.Path {
+				case "/api-testing/rest/v4/dummyHookId/tests/_exec":
+					tt.assertRequest(t, r)
+					completeStatusResp := tt.reply
+					_, _ = w.Write(completeStatusResp)
+				default:
+					w.WriteHeader(http.StatusInternalServerError)
+				}
+			}))
+			defer ts.Close()
+			c := &APITester{
+				HTTPClient: ts.Client(),
+				URL:        ts.URL,
+				Username:   "dummyUser",
+				AccessKey:  "dummyAccesKey",
+			}
+
+			got, err := c.RunEphemeralAsync(tt.args.ctx, tt.args.hookID, tt.args.buildID, tt.args.tunnel, tt.args.taskID, tt.args.test)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("RunAllAsync() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("RunAllAsync() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestAPITester_RunTestAsync(t *testing.T) {
+	type args struct {
+		ctx     context.Context
+		testID  string
+		hookID  string
+		buildID string
+		tunnel  config.Tunnel
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    AsyncResponse
+		wantErr bool
+	}{
+		{
+			name: "Basic trigger",
+			args: args{
+				ctx:    context.Background(),
+				hookID: "dummyHookId",
+				testID: "c20ad4d76fe97759aa27a0c9",
+			},
+			want: AsyncResponse{
+				ContextIDs: []string{"221270ac-0229-49d1-9025-251a10e9133d"},
+				EventIDs:   []string{"c4ca4238a0b923820dcc509a"},
+				TaskID:     "6ddf80b7-9753-4802-992b-d42948cdb99f",
+				TestIDs:    []string{"c20ad4d76fe97759aa27a0c9"},
+			},
+		},
+	}
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var err error
+		if r.Method != http.MethodPost {
+			w.WriteHeader(http.StatusNotImplemented)
+			return
+		}
+		switch r.URL.Path {
+		case "/api-testing/rest/v4/dummyHookId/tests/c20ad4d76fe97759aa27a0c9/_run":
+			completeStatusResp := []byte(`{"contextIds":["221270ac-0229-49d1-9025-251a10e9133d"],"eventIds":["c4ca4238a0b923820dcc509a"],"taskId":"6ddf80b7-9753-4802-992b-d42948cdb99f","testIds":["c20ad4d76fe97759aa27a0c9"]}`)
+			_, err = w.Write(completeStatusResp)
+		default:
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+
+		if err != nil {
+			t.Errorf("failed to respond: %v", err)
+		}
+	}))
+	defer ts.Close()
+	c := &APITester{
+		HTTPClient: ts.Client(),
+		URL:        ts.URL,
+		Username:   "dummyUser",
+		AccessKey:  "dummyAccesKey",
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := c.RunTestAsync(tt.args.ctx, tt.args.hookID, tt.args.testID, tt.args.buildID, tt.args.tunnel, TestRequest{})
+			if (err != nil) != tt.wantErr {
+				t.Errorf("RunAllAsync() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("RunAllAsync() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestAPITester_RunTagAsync(t *testing.T) {
+	type args struct {
+		ctx     context.Context
+		hookID  string
+		buildID string
+		tagID   string
+		tunnel  config.Tunnel
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    AsyncResponse
+		wantErr bool
+	}{
+		{
+			name: "Basic trigger",
+			args: args{
+				ctx:    context.Background(),
+				hookID: "dummyHookId",
+				tagID:  "dummyTag",
+			},
+			want: AsyncResponse{
+				ContextIDs: []string{"221270ac-0229-49d1-9025-251a10e9133d"},
+				EventIDs:   []string{"c4ca4238a0b923820dcc509a"},
+				TaskID:     "6ddf80b7-9753-4802-992b-d42948cdb99f",
+				TestIDs:    []string{"c20ad4d76fe97759aa27a0c9"},
+			},
+		},
+	}
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var err error
+		if r.Method != http.MethodPost {
+			w.WriteHeader(http.StatusNotImplemented)
+			return
+		}
+		switch r.URL.Path {
+		case "/api-testing/rest/v4/dummyHookId/tests/_tag/dummyTag/_run":
+			completeStatusResp := []byte(`{"contextIds":["221270ac-0229-49d1-9025-251a10e9133d"],"eventIds":["c4ca4238a0b923820dcc509a"],"taskId":"6ddf80b7-9753-4802-992b-d42948cdb99f","testIds":["c20ad4d76fe97759aa27a0c9"]}`)
+			_, err = w.Write(completeStatusResp)
+		default:
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+
+		if err != nil {
+			t.Errorf("failed to respond: %v", err)
+		}
+	}))
+	defer ts.Close()
+	c := &APITester{
+		HTTPClient: ts.Client(),
+		URL:        ts.URL,
+		Username:   "dummyUser",
+		AccessKey:  "dummyAccesKey",
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := c.RunTagAsync(tt.args.ctx, tt.args.hookID, tt.args.tagID, tt.args.buildID, tt.args.tunnel, TestRequest{})
+			if (err != nil) != tt.wantErr {
+				t.Errorf("RunAllAsync() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("RunAllAsync() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
