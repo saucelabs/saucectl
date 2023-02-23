8c1f594349ef9e43b50cb50d36ee7193f1306f6e
Alex Plischke <alex.plischke@saucelabs.com>
2023-02-23 15:08:20 -0800

    Remove dead code

--- internal/concurrency/concurrency.go
+++ internal/concurrency/concurrency.go
@@ -1,29 +1,5 @@
 package concurrency
 
-import (
-	"context"
-
-	"github.com/rs/zerolog/log"
-)
-
-// Min reads the allowed concurrency from r, compares it against ccy and returns the smaller value of the two.
-// A value of 1 is returned if r is unable to provide one.
-func Min(r Reader, ccy int) int {
-	allowed, err := r.ReadAllowedCCY(context.Background())
-	if err != nil {
-		log.Warn().Err(err).Msg("Unable to determine allowed concurrency. Using concurrency of 1.")
-		return 1
-	}
-
-	if ccy > allowed {
-		log.Warn().Msgf("Allowed concurrency is %d. Overriding configured value of %d.",
-			allowed, ccy)
-		return allowed
-	}
-
-	return ccy
-}
-
 // SplitTestFiles splits test files into groups to match concurrency
 func SplitTestFiles(files []string, concurrency int) [][]string {
 	if concurrency == 1 {
@@ -34,7 +10,7 @@ func SplitTestFiles(files []string, concurrency int) [][]string {
 	}
 	buckets := make([][]string, concurrency)
 	for i := 0; i < len(files); i++ {
-		buckets[i % concurrency] = append(buckets[i % concurrency], files[i])
+		buckets[i%concurrency] = append(buckets[i%concurrency], files[i])
 	}
 
 	return buckets
