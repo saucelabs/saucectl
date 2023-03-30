20cb17075d262f751aca4b899eb649a0e0c6b91a
Felix P <felix@saucelabs.com>
2023-03-30 13:26:44 +0200

    Enhance teting scope

--- internal/saucecloud/retry/appsretrier_test.go
+++ internal/saucecloud/retry/appsretrier_test.go
@@ -115,6 +115,79 @@ func TestAppsRetrier_Retry(t *testing.T) {
 				},
 			},
 		},
+		{
+			name: "Job not retrying if RDC and config is VDC + SmartRetry",
+			init: init{
+				RetryVDC: true,
+			},
+			args: args{
+				jobOpts: make(chan job.StartOptions),
+				opt: job.StartOptions{
+					DisplayName: "Dummy Test",
+					SmartRetry: job.SmartRetry{
+						FailedClassesOnly: true,
+					},
+					TestOptions: map[string]interface{}{
+						"class": []string{"Demo.Class1", "Demo.Class2", "Demo.Class3"},
+					},
+				},
+				previous: job.Job{
+					ID:    "fake-job-id",
+					IsRDC: true,
+				},
+			},
+			expected: job.StartOptions{
+				DisplayName: "Dummy Test",
+				TestOptions: map[string]interface{}{
+					"class": []string{"Demo.Class1", "Demo.Class2", "Demo.Class3"},
+				},
+				SmartRetry: job.SmartRetry{
+					FailedClassesOnly: true,
+				},
+			},
+		},
+		{
+			name: "Job is retrying when VDC + SmartRetry",
+			init: init{
+				VDCReader: &mocks.FakeJobReader{
+					ReadJobFn:              nil,
+					PollJobFn:              nil,
+					GetJobAssetFileNamesFn: nil,
+					GetJobAssetFileContentFn: func(ctx context.Context, jobID, fileName string) ([]byte, error) {
+						if jobID == "fake-job-id" && fileName == junit.JunitFileName {
+							return []byte("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n<testsuite>\n    <testcase classname=\"Demo.Class1\">\n        <failure>ERROR</failure>\n    </testcase>\n    <testcase classname=\"Demo.Class1\"/>\n    <testcase classname=\"Demo.Class2\"/>\n    <testcase classname=\"Demo.Class3\"/>\n</testsuite>\n"), nil
+						}
+						return []byte{}, errors.New("unknown file")
+					},
+				},
+				RetryVDC: true,
+			},
+			args: args{
+				jobOpts: make(chan job.StartOptions),
+				opt: job.StartOptions{
+					DisplayName: "Dummy Test",
+					SmartRetry: job.SmartRetry{
+						FailedClassesOnly: true,
+					},
+					TestOptions: map[string]interface{}{
+						"class": []string{"Demo.Class1", "Demo.Class2", "Demo.Class3"},
+					},
+				},
+				previous: job.Job{
+					ID:    "fake-job-id",
+					IsRDC: false,
+				},
+			},
+			expected: job.StartOptions{
+				DisplayName: "Dummy Test",
+				TestOptions: map[string]interface{}{
+					"class": []string{"Demo.Class1"},
+				},
+				SmartRetry: job.SmartRetry{
+					FailedClassesOnly: true,
+				},
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
