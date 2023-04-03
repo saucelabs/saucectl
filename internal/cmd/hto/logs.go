5dc5a8ada0b92ea4f6af6b46a3e49244f7b9e41b
Mike Han <mike.han@saucelabs.com>
2023-04-03 10:27:17 -0600

    add prerun for segment tracking

--- internal/cmd/hto/logs.go
+++ internal/cmd/hto/logs.go
@@ -2,15 +2,33 @@ package hto
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
+	cmds "github.com/saucelabs/saucectl/internal/cmd"
+	"github.com/saucelabs/saucectl/internal/imagerunner"
+	"github.com/saucelabs/saucectl/internal/segment"
+	"github.com/saucelabs/saucectl/internal/usage"
 	"github.com/spf13/cobra"
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
 )
 
 func LogsCommand() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "logs <runID>",
 		Short: "Fetch the logs for an HTO run",
+		PreRun: func(cmd *cobra.Command, args []string) {
+			tracker := segment.DefaultTracker
+
+			go func() {
+				tracker.Collect(
+					cases.Title(language.English).String(cmds.FullName(cmd)),
+					usage.Properties{}.SetFlags(cmd.Flags()),
+				)
+				_ = tracker.Close()
+			}()
+		},
 		RunE: func(cmd *cobra.Command, args []string) error {
 			return exec(args[0])
 		},
