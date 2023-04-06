9fb92c2461b5522a7847991e27466bbcaec3ab2c
Felix P <felix@saucelabs.com>
2023-04-06 22:14:47 +0200

    Enforce http:// https:// and hide password

--- internal/http/proxy.go
+++ internal/http/proxy.go
@@ -7,12 +7,18 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"strings"
 )
 
 // CheckHTTPProxy checks that the HTTP_PROXY is valid if it exists.
 func CheckHTTPProxy() error {
 	proxyURL := os.Getenv("HTTP_PROXY")
 	if proxyURL != "" {
+		if !strings.HasPrefix(proxyURL, "http://") && !strings.HasPrefix(proxyURL, "https://") {
+			color.Red("\nA proxy has been set, but its url is invalid !\n\n")
+			fmt.Printf("HTTP_PROXY must start by http:// or https:// ")
+			return fmt.Errorf("invalid HTTP_PROXY value")
+		}
 		url, err := url.Parse(proxyURL)
 		if err != nil {
 			color.Red("\nA proxy has been set, but its url is invalid !\n\n")
@@ -20,7 +26,14 @@ func CheckHTTPProxy() error {
 			println()
 			return fmt.Errorf("invalid HTTP_PROXY value")
 		}
-		log.Info().Msgf(fmt.Sprintf("Using proxy: %s://%s:%s", url.Scheme, url.Hostname(), url.Port()))
+		displayUrl := proxyURL
+		if url.User != nil {
+			pass, hasPass := url.User.Password()
+			if hasPass {
+				displayUrl = strings.Replace(displayUrl, pass, "***", -1)
+			}
+		}
+		log.Info().Msgf(fmt.Sprintf("Using proxy: %s", displayUrl))
 	}
 	return nil
 }
