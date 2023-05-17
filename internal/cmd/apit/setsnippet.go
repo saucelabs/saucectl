3420b4f02ad0f588901de08a1d90eba9ed4b1a67
Mike Han <mike.han@saucelabs.com>
2023-05-17 11:28:29 -0600

    update set-snippet

--- internal/cmd/apit/setsnippet.go
+++ internal/cmd/apit/setsnippet.go
@@ -13,14 +13,20 @@ import (
 )
 
 func SetSnippetCommand() *cobra.Command {
-	var file string
-	var project string
 	cmd := &cobra.Command{
-		Use:          "set-snippet <snippetName>",
-		Short:        "Set a vault snippet",
+		Use:   "set-snippet NAME FILE_NAME|- [--project PROJECT_NAME]",
+		Short: "Set a vault snippet",
+		Long: `
+Set/update a snippet in a project's vault. If a snippet NAME is already in the vault,
+the value will be updated otherwise a new snippet will be added. You can set a snippet's
+value by providing a path to a file defining the snippet or use "-" to read from stdin.
+
+Use [--project] to specify a project by its name or run without [--project] to choose from
+a list of projects
+`,
 		SilenceUsage: true,
 		Args: func(cmd *cobra.Command, args []string) error {
-			if len(args) == 0 || (args[0] == "") {
+			if len(args) == 0 || (args[0] == "" || args[1] == "") {
 				// TODO: Give useful error message
 				return errors.New("no project name specified")
 			}
@@ -44,47 +50,43 @@ func SetSnippetCommand() *cobra.Command {
 			return nil
 		},
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return setSnippet(project, args[0], file)
+			name := args[0]
+			fileName := args[1]
+
+			b, err := readSnippet(fileName)
+			if err != nil {
+				return err
+			}
+
+			updateVault := apitest.Vault{
+				Variables: []apitest.VaultVariable{},
+				Snippets: map[string]string{
+					name: string(b),
+				},
+			}
+
+			err = apitesterClient.PutVault(context.Background(), selectedProject.Hooks[0].Identifier, updateVault)
+			if err != nil {
+				return err
+			}
+			return nil
 		},
 	}
-	cmd.Flags().StringVar(&project, "project", "", "The name of the project.")
-	cmd.Flags().StringVar(&file, "file", "", "A file that defines a vault snippet. Use '-' to read from stdin.")
-	cmd.MarkFlagRequired("file")
-	cmd.MarkFlagRequired("project")
 
 	return cmd
 }
 
-func setSnippet(projectName string, name string, fileName string) error {
+func readSnippet(fileName string) ([]byte, error) {
 	var r io.Reader
 	var err error
 	if fileName == "-" {
 		r = os.Stdin
 	} else {
 		r, err = os.Open(fileName)
+		if err != nil {
+			return []byte{}, err
+		}
 	}
 
-	if err != nil {
-		return err
-	}
-
-	b, err := io.ReadAll(r)
-
-	project, err := resolve(projectName)
-	if err != nil {
-		return err
-	}
-
-	updateVault := apitest.Vault{
-		Variables: []apitest.VaultVariable{},
-		Snippets: map[string]string{
-			name: string(b),
-		},
-	}
-
-    err = apitesterClient.PutVault(context.Background(), project.Hooks[0].Identifier, updateVault)
-    if err != nil {
-    	return err
-    }
-	return nil
+	return io.ReadAll(r)
 }
