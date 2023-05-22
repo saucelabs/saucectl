4cac27d0637550563b19939609588e6bee49ed91
Felix P <felix@saucelabs.com>
2023-05-22 15:56:30 +0200

    Add output for Github Summary

--- internal/cmd/run/run.go
+++ internal/cmd/run/run.go
@@ -33,6 +33,7 @@ import (
 	"github.com/saucelabs/saucectl/internal/report"
 	"github.com/saucelabs/saucectl/internal/report/buildtable"
 	"github.com/saucelabs/saucectl/internal/report/captor"
+	"github.com/saucelabs/saucectl/internal/report/github"
 	"github.com/saucelabs/saucectl/internal/report/json"
 	"github.com/saucelabs/saucectl/internal/testcafe"
 	"github.com/saucelabs/saucectl/internal/version"
@@ -286,10 +287,12 @@ func checkForUpdates() {
 func createReporters(c config.Reporters, ntfs config.Notifications, metadata config.Metadata,
 	svc slack.Service, buildReader build.Reader, framework, env string) []report.Reporter {
 	buildReporter := buildtable.New(buildReader)
+	githubReporter := github.NewGithubSummary()
 
 	reps := []report.Reporter{
 		&captor.Default,
 		&buildReporter,
+		&githubReporter,
 	}
 
 	if c.JUnit.Enabled {
---
+++ internal/report/github/summary.go
@@ -0,0 +1,69 @@
+package github
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/saucelabs/saucectl/internal/report"
+)
+
+type Reporter struct {
+	stepSummaryFile string
+	results         []report.TestResult
+}
+
+func NewGithubSummary() Reporter {
+	return Reporter{
+		stepSummaryFile: os.Getenv("GITHUB_STEP_SUMMARY"),
+	}
+}
+
+func (r *Reporter) isActive() bool {
+	return r.stepSummaryFile != ""
+}
+
+func (r *Reporter) Add(t report.TestResult) {
+	if !r.isActive() {
+		return
+	}
+	r.results = append(r.results, t)
+}
+
+func (r *Reporter) Render() {
+	if !r.isActive() {
+		return
+	}
+	fd, err := os.Open("/dev/stdout")
+	if err != nil {
+		return
+	}
+	fmt.Println("Rendering")
+	renderHeader(fd)
+	for _, result := range r.results {
+		renderTestResult(fd, result)
+	}
+}
+
+func (r *Reporter) Reset() {
+	if !r.isActive() {
+		return
+	}
+}
+
+func (r *Reporter) ArtifactRequirements() []report.ArtifactType {
+	return []report.ArtifactType{}
+}
+
+func renderHeader(f *os.File) {
+	fmt.Fprint(f, "| | Name | Duration | Status | Browser | Platform | Device |\n")
+	fmt.Fprint(f, "| --- | --- | --- | --- | --- | --- | --- |\n")
+}
+
+func renderTestResult(f *os.File, t report.TestResult) {
+	mark := ":x:"
+	if t.Status == "passed" {
+		mark = ":white_check_mark:"
+	}
+	fmt.Fprintf(f, "| %s | [%s](%s) | %.0fs | %s | %s | %s | %s |\n",
+		mark, t.Name, t.URL, t.Duration.Seconds(), t.Status, t.Browser, t.Platform, t.DeviceName)
+}
