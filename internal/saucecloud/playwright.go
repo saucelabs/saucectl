262202c828e01c9894fb234289be49853edcb983
Felix P <felix@saucelabs.com>
2023-05-24 15:28:14 +0200

    Propagate to other frameworks

--- internal/saucecloud/playwright.go
+++ internal/saucecloud/playwright.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"strings"
 
 	"github.com/rs/zerolog/log"
 	"github.com/saucelabs/saucectl/internal/framework"
@@ -68,7 +67,7 @@ func (r *PlaywrightRunner) RunProject() (int, error) {
 	}
 
 	if r.Project.DryRun {
-		log.Info().Msgf("The following test suites would have run: [%s].", r.getSuiteNames())
+		printDryRunSuiteNames(r.getSuiteNames())
 		return 0, nil
 	}
 
@@ -84,13 +83,12 @@ func (r *PlaywrightRunner) RunProject() (int, error) {
 	return exitCode, nil
 }
 
-func (r *PlaywrightRunner) getSuiteNames() string {
+func (r *PlaywrightRunner) getSuiteNames() []string {
 	var names []string
 	for _, s := range r.Project.Suites {
 		names = append(names, s.Name)
 	}
-
-	return strings.Join(names, ", ")
+	return names
 }
 
 func (r *PlaywrightRunner) runSuites(app string, otherApps []string) bool {
