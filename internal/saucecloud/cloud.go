5927b6a22f8e16ebf38324346a5a1bf8c0c49289
Tian Feng <tian.feng@saucelabs.com>
2023-07-26 09:17:25 -0700

    Update internal/saucecloud/cloud.go
    Co-authored-by: Alex Plischke <alex.plischke@saucelabs.com>

--- internal/saucecloud/cloud.go
+++ internal/saucecloud/cloud.go
@@ -147,12 +147,11 @@ func (r *CloudRunner) collectResults(artifactCfg config.ArtifactDownload, result
 			artifacts = append(artifacts, junitArtifact)
 
 			files := r.downloadArtifacts(res.name, res.job, artifactCfg.When)
-			if report.IsArtifactRequired(r.Reporters, report.JSONArtifact) {
-				for _, f := range files {
-					artifacts = append(artifacts, report.Artifact{
-						FilePath: f,
-					})
-				}
+		              for _, f := range files {
+			              artifacts = append(artifacts, report.Artifact{
+				              FilePath: f,
+			              })
+		              }
 			}
 
 			var url string
