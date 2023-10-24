cd822374b15c681ed367e49c471155e61df2c910
seb vincent <seb.vincent@gmail.com>
2023-10-24 21:36:44 +0800

    fix: canonicalize service name correctly (#844)

--- internal/imagerunner/utils.go
+++ internal/imagerunner/utils.go
@@ -6,12 +6,15 @@ import (
 )
 
 func GetCanonicalServiceName(serviceName string) string {
+	serviceName = strings.TrimSpace(serviceName)
 	if serviceName == "" {
 		return ""
 	}
 	// make sure the service name has only lowercase letters, numbers, and underscores
-	canonicalName := strings.ToLower(regexp.MustCompile("[^a-z0-9-]").ReplaceAllString(serviceName, "-"))
+	canonicalName := strings.ToLower(regexp.MustCompile("[^a-zA-Z0-9-]").ReplaceAllString(serviceName, "-"))
 	// remove successives dashes
 	canonicalName = regexp.MustCompile("-+").ReplaceAllString(canonicalName, "-")
+	// avoids container names starting with dash
+	canonicalName = regexp.MustCompile("^-").ReplaceAllString(canonicalName, "s-")
 	return canonicalName
 }
