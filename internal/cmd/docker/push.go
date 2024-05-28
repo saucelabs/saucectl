77a9777bf32cd4b41d1397c342d16196afd56b43
Tian Feng <tian.feng@saucelabs.com>
2024-05-28 13:06:57 -0700

    feat: Use progress bar to display docker output (#913)
    * feat: Use progress bar to display docker output
    
    * make linter happy
    
    * construct docker own progress bar
    
    * wrap bar functions
    
    * use another spinner
    
    * cleanup
    
    * use spinner and bar both
    
    * update condition
    
    * refine comment
    
    * update the condition
    
    * revert bar to nil
    
    * add comment
    
    * identify each by id instead of status
    
    * revise comment
    
    * revise comment
    
    * Speed estimation is incorrect when in parallel.
    
    * group output by ID
    
    * Update internal/cmd/docker/push.go
    
    Co-authored-by: Alex Plischke <alex.plischke@saucelabs.com>
    
    ---------
    
    Co-authored-by: Alex Plischke <alex.plischke@saucelabs.com>

--- internal/cmd/docker/push.go
+++ internal/cmd/docker/push.go
@@ -13,9 +13,12 @@ import (
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/registry"
 	"github.com/docker/docker/client"
+	dockermsg "github.com/moby/moby/pkg/jsonmessage"
 	cmds "github.com/saucelabs/saucectl/internal/cmd"
+	"github.com/saucelabs/saucectl/internal/progress"
 	"github.com/saucelabs/saucectl/internal/segment"
 	"github.com/saucelabs/saucectl/internal/usage"
+	"github.com/schollz/progressbar/v3"
 	"github.com/spf13/cobra"
 	"golang.org/x/text/cases"
 	"golang.org/x/text/language"
@@ -96,11 +99,92 @@ func pushDockerImage(imageName, username, password string, timeout time.Duration
 		return nil
 	}
 
-	// Print the push output
-	_, err = io.Copy(os.Stdout, out)
-	if err != nil {
-		return fmt.Errorf("docker output: %v", err)
+	return logPushProgress(out)
+}
+
+func logPushProgress(reader io.ReadCloser) error {
+	var stepID string
+	bars := map[string]*progressbar.ProgressBar{}
+	decoder := json.NewDecoder(reader)
+	for {
+		// Decode the message from the Docker API output.
+		var msg dockermsg.JSONMessage
+		err := decoder.Decode(&msg)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return fmt.Errorf("failed to decode JSON during Docker image push: %v", err)
+		}
+		if msg.Error != nil {
+			return fmt.Errorf("server error during Docker image push: %s", msg.Error.Message)
+		}
+
+		// Create a new progress spinner or bar when the Docker push ID changes.
+		// Each ID corresponds to a distinct step in the push process.
+		// Note: Outputs are in parallel; identical IDs indicate outputs from the same thread.
+		if stepID != msg.ID {
+			stepID = msg.ID
+			progress.Stop()
+
+			// Create a progress spinner for statuses that don't have progress details, like 'Preparing'.
+			if msg.Progress == nil || msg.Progress.Total == 0 {
+				progress.Show(msg.Status)
+				continue
+			}
+
+			// Init a progress bar for 'Pushing' status with total bytes and add it to bar group.
+			if _, ok := bars[msg.ID]; !ok {
+				bars[msg.ID] = createBar(msg.Progress.Total, fmt.Sprintf("%s %s", msg.Status, msg.ID))
+			}
+		}
+
+		// Update current progress based on msg.Progress.Total when in 'Pushing' status.
+		// Note: The Docker API may return a current value greater than total. To prevent breaking the progress bar,
+		// only update when the current is less than total.
+		if bar, ok := bars[msg.ID]; ok {
+			if msg.Progress != nil && msg.Progress.Current > 0 && msg.Progress.Current < bar.GetMax64() {
+				_ = bar.Set64(msg.Progress.Current)
+			}
+		}
+	}
+	if err := closeProgress(bars); err != nil {
+		return err
 	}
 
+	fmt.Println("Successfully pushed the Docker image!")
+	return nil
+}
+
+// createBar returns a customized progress bar for Docker image pushes.
+func createBar(max int64, desc string) *progressbar.ProgressBar {
+	return progressbar.NewOptions64(
+		max,
+		progressbar.OptionSetDescription(desc),
+		progressbar.OptionSetWriter(os.Stderr),
+		progressbar.OptionShowBytes(true),
+		progressbar.OptionOnCompletion(func() {
+			fmt.Fprint(os.Stderr, "\n")
+		}),
+		progressbar.OptionFullWidth(),
+		progressbar.OptionSetRenderBlankState(true),
+		progressbar.OptionSetTheme(progressbar.Theme{
+			Saucer:        "=",
+			SaucerHead:    ">",
+			SaucerPadding: " ",
+			BarStart:      "[",
+			BarEnd:        ">]",
+		}),
+	)
+}
+
+// closeProgress closes all progress spinners and bars.
+func closeProgress(bars map[string]*progressbar.ProgressBar) error {
+	progress.Stop()
+	for _, bar := range bars {
+		if err := bar.Finish(); err != nil {
+			return err
+		}
+	}
 	return nil
 }
