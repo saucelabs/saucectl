d10adf26926e480f36470ea1e9f41bb83f7a77c4
Mike Han <mike.han@saucelabs.com>
2024-08-16 10:04:35 -0600

    Move test to new package

--- internal/http/rdcservice_test.go
+++ internal/http/rdcservice_test.go
@@ -350,50 +350,6 @@ func TestRDCService_GetJobAssetFileContent(t *testing.T) {
 	}
 }
 
-// func TestRDCService_DownloadArtifact(t *testing.T) {
-// 	fileContent := "<xml>junit.xml</xml>"
-// 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-// 		var err error
-// 		switch r.URL.Path {
-// 		case "/v1/rdc/jobs/test-123":
-// 			_, err = w.Write([]byte(`{"automation_backend":"espresso"}`))
-// 		case "/v1/rdc/jobs/test-123/junit.xml":
-// 			_, err = w.Write([]byte(fileContent))
-// 		default:
-// 			w.WriteHeader(http.StatusNotFound)
-// 		}
-// 
-// 		if err != nil {
-// 			t.Errorf("failed to respond: %v", err)
-// 		}
-// 	}))
-// 	defer ts.Close()
-// 
-// 	tempDir, err := os.MkdirTemp("", "saucectl-download-artifact")
-// 	if err != nil {
-// 		t.Errorf("Failed to create temp dir: %v", err)
-// 	}
-// 	defer func() {
-// 		_ = os.RemoveAll(tempDir)
-// 	}()
-// 
-// 	rc := NewRDCService(ts.URL, "dummy-user", "dummy-key", 10*time.Second, config.ArtifactDownload{
-// 		Directory: tempDir,
-// 		Match:     []string{"junit.xml"},
-// 	})
-// 	rc.DownloadArtifact("test-123", "suite name", true)
-// 
-// 	fileName := filepath.Join(tempDir, "suite_name", "junit.xml")
-// 	d, err := os.ReadFile(fileName)
-// 	if err != nil {
-// 		t.Errorf("file '%s' not found: %v", fileName, err)
-// 	}
-// 
-// 	if string(d) != fileContent {
-// 		t.Errorf("file content mismatch: got '%v', expects: '%v'", d, fileContent)
-// 	}
-// }
-
 func TestRDCService_GetDevices(t *testing.T) {
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		var err error
---
+++ internal/saucecloud/downloader/downloader_test.go
@@ -0,0 +1,60 @@
+package downloader
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/saucelabs/saucectl/internal/config"
+	httpServices "github.com/saucelabs/saucectl/internal/http"
+)
+
+func TestArtifactDownloader_DownloadArtifact(t *testing.T) {
+ 	fileContent := "<xml>junit.xml</xml>"
+ 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+ 		var err error
+ 		switch r.URL.Path {
+ 		case "/v1/rdc/jobs/test-123":
+ 			_, err = w.Write([]byte(`{"automation_backend":"espresso"}`))
+ 		case "/v1/rdc/jobs/test-123/junit.xml":
+ 			_, err = w.Write([]byte(fileContent))
+ 		default:
+ 			w.WriteHeader(http.StatusNotFound)
+ 		}
+ 
+ 		if err != nil {
+ 			t.Errorf("failed to respond: %v", err)
+ 		}
+ 	}))
+ 	defer ts.Close()
+ 
+ 	tempDir, err := os.MkdirTemp("", "saucectl-download-artifact")
+ 	if err != nil {
+ 		t.Errorf("Failed to create temp dir: %v", err)
+ 	}
+ 	defer func() {
+ 		_ = os.RemoveAll(tempDir)
+ 	}()
+ 
+ 	rc := httpServices.NewRDCService(ts.URL, "dummy-user", "dummy-key", 10*time.Second)
+	artifactCfg := config.ArtifactDownload{
+ 		Directory: tempDir,
+ 		Match:     []string{"junit.xml"},
+ 	}
+
+	downloader := NewArtifactDownloader(&rc, artifactCfg)
+ 	downloader.DownloadArtifact("test-123", "suite name", true)
+ 
+ 	fileName := filepath.Join(tempDir, "suite_name", "junit.xml")
+ 	d, err := os.ReadFile(fileName)
+ 	if err != nil {
+ 		t.Errorf("file '%s' not found: %v", fileName, err)
+ 	}
+ 
+ 	if string(d) != fileContent {
+ 		t.Errorf("file content mismatch: got '%v', expects: '%v'", d, fileContent)
+ 	}
+}
