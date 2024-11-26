3f05b0a82b9f9f29d53dd48650520d4632c838dd
Tian Feng <tian.feng@saucelabs.com>
2024-11-26 15:43:04 -0800

    feat: rename CI tags

--- internal/insights/testrun.go
+++ internal/insights/testrun.go
@@ -204,10 +204,10 @@ func enrichInsightTestRun(runs []TestRun, jobID string, jobName string, details 
 
 		if provider != ci.None {
 			runs[idx].CI = &CI{
-				Branch:     ciData.RefName,
-				RefName:    ciData.RefName,
+				Branch:     ciData.Ref,
+				RefName:    ciData.Ref,
 				Repository: ciData.Repo,
-				CommitSha:  ciData.SHA,
+				CommitSha:  ciData.ShortSHA,
 			}
 		}
 	}
